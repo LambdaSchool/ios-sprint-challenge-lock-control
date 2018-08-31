590f9452a1a5aa44a5527321baa13ef73bc435ca
Jeremy Taylor <jtaylor@jctaylor.org>
2018-08-31 12:19:56 -0500

    Unlocking and locking via reset works. I don't have a thumbview yet

--- Sprint 6 Challenge/Sprint 6 Challenge/LockControl.swift
+++ Sprint 6 Challenge/Sprint 6 Challenge/LockControl.swift
@@ -11,8 +11,11 @@ import UIKit
 class LockControl: UIControl {
     
     var isUnlocked = true
+    var progress: Float = 0.0
+    
     
     override func beginTracking(_ touch: UITouch, with event: UIEvent?) -> Bool {
+        updateValue(at: touch)
         sendActions(for: .touchDown)
         return true
     }
@@ -20,6 +23,7 @@ class LockControl: UIControl {
     override func continueTracking(_ touch: UITouch, with event: UIEvent?) -> Bool {
         let touchPoint = touch.location(in: self)
         if bounds.contains(touchPoint) {
+            updateValue(at: touch)
             sendActions(for: .touchDragInside)
         } else {
             sendActions(for: .touchDragOutside)
@@ -33,6 +37,7 @@ class LockControl: UIControl {
         guard let touch = touch else { return }
         let touchPoint = touch.location(in: self)
         if bounds.contains(touchPoint) {
+            updateValue(at: touch)
             sendActions(for: .touchUpInside)
         } else {
             sendActions(for: .touchUpOutside)
@@ -41,6 +46,17 @@ class LockControl: UIControl {
     
     override func cancelTracking(with event: UIEvent?) {
         sendActions(for: .touchCancel)
+    }
+    
+    func updateValue(at touch: UITouch) {
+        let touchPoint = touch.location(in: self)
+        let x = touchPoint.x
+        progress = Float(min(x/self.bounds.size.width, 1))
+        print(progress)
+        if progress >= 0.80 {
+            isUnlocked = true
+            sendActions(for: .valueChanged)
+        }
     }
     
 }

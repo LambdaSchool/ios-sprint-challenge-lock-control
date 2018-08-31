415dfb2c08d9cb6e769ecc5f7079ac608fb81d2c
Conner Alegre <cga0@pm.me>
2018-08-31 12:01:32 -0600

    incomplete project, nothing done

--- Sprint 6 Challenge/Sprint 6 Challenge/SliderControl.swift
+++ Sprint 6 Challenge/Sprint 6 Challenge/SliderControl.swift
@@ -19,16 +19,44 @@ class SliderControl: UIControl {
     super.init(coder: aCoder)
     setup()
   }
-
+  
   private let sliderControl = UILabel(frame: CGRect(x: 0, y: 0, width: 40, height: 41))
   private var progress = 0.0
   
-  func setup() {
-    self.backgroundColor = UIColor.red
+  private func setup() {
+    self.backgroundColor = UIColor.lightGray
     
     sliderControl.textAlignment = .center
     sliderControl.backgroundColor = .black
     
+    
+    let panGesture = UIPanGestureRecognizer(target: self, action: #selector(self.pan(_:)))
+    self.addGestureRecognizer(panGesture)
     self.addSubview(sliderControl)
+//    sliderControl.translatesAutoresizingMaskIntoConstraints = false
+    let horizontalConstraint = NSLayoutConstraint(item: sliderControl,
+                                                  attribute: .leading,
+                                                  relatedBy: .equal,
+                                                  toItem: sliderControl,
+                                                  attribute: .leading,
+                                                  multiplier: 1,
+                                                  constant: 10)
+    
+    NSLayoutConstraint.activate([horizontalConstraint])
   }
+  
+  @objc func pan(_ sender: UIPanGestureRecognizer) {
+    let x = sender.location(in: self).x
+    print(x)
+    
+    switch sender.state {
+    case .began:
+      self.sendActions(for: .editingDidBegin)
+    case .changed:
+      self.sendActions(for: .valueChanged)
+    default:
+      break
+    }
+  }
+  
 }

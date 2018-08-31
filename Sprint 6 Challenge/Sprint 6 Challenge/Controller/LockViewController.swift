164cfbf810e64c8d82fc8a34f439ab787b6b3fef
Lambda-School-Loaner-11 <lambda-school-loaner-11@Lambda-School-Loaner-11s-MacBook-Pro.local>
2018-08-31 10:41:19 -0700

    update unlock controller

--- Sprint 6 Challenge/Sprint 6 Challenge/Controller/LockViewController.swift
+++ Sprint 6 Challenge/Sprint 6 Challenge/Controller/LockViewController.swift
@@ -14,74 +14,25 @@ class LockViewController: UIViewController, UIViewControllerTransitioningDelegat
     
     let animator = Animator()
     
-    // Transition object
-    
-    private var interactionController: UIPercentDrivenInteractiveTransition?
-    
     // Lock image on transition
     
     @IBAction func reset(_ sender: Any) {
         transitioningDelegate = self
-        lockImage.image = lockedImage.image
+        dismiss(animated: true, completion: nil)
+        
     }
-    
-    // Image references
-    
-    let lockedImage: UIImageView = {
-        let image = UIImageView()
-        image.image = UIImage(named: "Locked")
-        image.translatesAutoresizingMaskIntoConstraints = false
-        return image
-    }()
-    
-    let unlockedImage: UIImageView = {
-        let image = UIImageView()
-        image.image = UIImage(named: "Unlocked")
-        image.translatesAutoresizingMaskIntoConstraints = false
-        return image
-    }()
     
     @IBOutlet weak var lockImage: UIImageView!
     
-    
-    // Track transition state and return values
-    
-    @IBAction func panGesture(_ sender: UIPanGestureRecognizer) {
-        
-        switch sender.state {
-        case .began:
-            let location = sender.location(in: lockImage)
-            if location.x < lockImage.bounds.minX {
-                interactionController = UIPercentDrivenInteractiveTransition()
-                reset(sender)
-            }
-            lockImage.image = lockedImage.image
-        case .changed:
-            let translation = sender.translation(in: lockImage)
-            let percentageComplete = fabs(translation.x / lockImage.bounds.width)
-            interactionController?.update(percentageComplete)
-            lockImage.image = lockedImage.image
-        case .ended:
-            if sender.velocity(in: lockImage).x > 0 {
-                interactionController?.finish()
-            } else {
-                interactionController?.cancel()
-            }
-            interactionController = nil
-            lockImage.image = unlockedImage.image
-        default:
-            break
-        }
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        segue.destination.transitioningDelegate = self
     }
     
-    func animationController(forDismissed dismissed: UIViewController) -> UIViewControllerAnimatedTransitioning? {
+    // Delegate for presenting
+    
+    func animationController(forPresented presented: UIViewController, presenting: UIViewController, source: UIViewController) -> UIViewControllerAnimatedTransitioning? {
         
         return animator
-    }
-    
-    func interactionControllerForDismissal(using animator: UIViewControllerAnimatedTransitioning) -> UIViewControllerInteractiveTransitioning? {
-        
-        return interactionController
     }
     
     override func viewDidLoad() {
--- Sprint 6 Challenge/Sprint 6 Challenge/Controller/UnlockViewController.swift
+++ Sprint 6 Challenge/Sprint 6 Challenge/Controller/UnlockViewController.swift
@@ -8,10 +8,51 @@
 
 import UIKit
 
-class UnlockViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
+class UnlockViewController: UIViewController, UIViewControllerTransitioningDelegate {
+    
+    let animator = Animator()
+    
+    private var interactionController: UIPercentDrivenInteractiveTransition?
+    
+    @IBOutlet weak var unlockImage: UIImageView!
+    
+    @IBAction func reset(_ sender: Any) {
+        transitioningDelegate = self
+        dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func panGesture(_ sender: UIPanGestureRecognizer) {
+        switch sender.state {
+        case .began:
+            let location = sender.location(in: unlockImage)
+            if location.x < unlockImage.bounds.minX {
+                interactionController = UIPercentDrivenInteractiveTransition()
+                reset(sender)
+            }
+        case .changed:
+            let translation = sender.translation(in: unlockImage)
+            let percentageComplete = fabs(translation.x / unlockImage.bounds.width)
+            interactionController?.update(percentageComplete)
+        case .ended:
+            if sender.velocity(in: unlockImage).x > 0 {
+                interactionController?.finish()
+            } else {
+                interactionController?.cancel()
+            }
+            interactionController = nil
+        default:
+            break
+        }
+    }
+    
+    
+    func animationController(forDismissed dismissed: UIViewController) -> UIViewControllerAnimatedTransitioning? {
+        
+        return animator
+    }
+    
+    func interactionControllerForDismissal(using animator: UIViewControllerAnimatedTransitioning) -> UIViewControllerInteractiveTransitioning? {
+        
+        return interactionController
     }
 }

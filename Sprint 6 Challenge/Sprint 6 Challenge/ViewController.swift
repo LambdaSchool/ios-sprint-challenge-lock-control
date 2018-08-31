7cf535a46e6b2e4e2aa9862ab4d9479c6f1c8dde
Samantha Gatt <smfgatt@gmail.com>
2018-08-31 13:03:12 -0400

    Implements toggling isLocked property of main view controller

--- Sprint 6 Challenge/Sprint 6 Challenge/ViewController.swift
+++ Sprint 6 Challenge/Sprint 6 Challenge/ViewController.swift
@@ -13,8 +13,29 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+//        lockButton.title = ""
+//        lockButton.isEnabled = false
     }
 
     
+    var isLocked: Bool = true
+    @IBOutlet weak var lockButton: UIBarButtonItem!
+    @IBOutlet weak var image: UIImageView!
+    
+    
+    @IBAction func lock(_ sender: Any) {
+        toggleLocked()
+    }
+    
+    
+    func toggleLocked() {
+        if self.isLocked {
+            self.isLocked = !self.isLocked
+            self.image.image = UIImage(named: "Unlocked")
+        } else {
+            self.isLocked = !self.isLocked
+            self.image.image = UIImage(named: "Locked")
+        }
+    }
 }
 

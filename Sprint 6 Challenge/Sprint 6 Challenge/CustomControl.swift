28095bcd7e96d9e0233917781322d5677d7fef72
Carolyn Lea <carolynrlea@gmail.com>
2018-08-31 09:33:39 -0600

    Initial commit: basic setup

---
+++ Sprint 6 Challenge/Sprint 6 Challenge/CustomControl.swift
@@ -0,0 +1,14 @@
+//
+//  CustomControl.swift
+//  Sprint 6 Challenge
+//
+//  Created by Carolyn Lea on 8/31/18.
+//  Copyright © 2018 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+class CustomControl: UIControl
+{
+    
+}

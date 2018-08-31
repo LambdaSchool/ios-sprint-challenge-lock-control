71519de412942ac6818b4ea7dd546e433d85579b
Andrew Liao <andrewoliao@gmail.com>
2018-08-31 08:41:01 -0700

    Set up storyboards and linked actions to ViewController.swift

---
+++ Sprint 6 Challenge/Sprint 6 Challenge/LockControl.swift
@@ -0,0 +1,14 @@
+//
+//  LockControl.swift
+//  Sprint 6 Challenge
+//
+//  Created by Andrew Liao on 8/31/18.
+//  Copyright © 2018 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+class LockControl: UIControl {
+    
+    var isLocked: Bool = true
+}
--- Sprint 6 Challenge/Sprint 6 Challenge/ViewController.swift
+++ Sprint 6 Challenge/Sprint 6 Challenge/ViewController.swift
@@ -2,24 +2,31 @@
 //  ViewController.swift
 //  Sprint 6 Challenge
 //
-//  Created by Spencer Curtis on 8/29/18.
+//  Created by Andrew Liao on 8/31/18.
 //  Copyright © 2018 Lambda School. All rights reserved.
 //
 
 import UIKit
 
 class ViewController: UIViewController {
-
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        if lockControl.isLocked && isViewLoaded{
+            resetButton.title = ""
+            self.title = "Locked"
+        }
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func unlock(_ lockControl: LockControl) {
+        if lockControl.isLocked{
+            resetButton.title = "Reset"
+            self.title = "Unlocked"
+        }
     }
-
-
+    @IBAction func reset(_ sender: Any) {
+        
+    }
+    
+    @IBOutlet weak var resetButton: UIBarButtonItem!
+    @IBOutlet weak var lockControl: LockControl!
 }
-

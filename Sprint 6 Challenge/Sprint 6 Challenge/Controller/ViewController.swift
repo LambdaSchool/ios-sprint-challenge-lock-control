ff45a42398e6a535025f8dd548148c98fd03f3b1
Lambda-School-Loaner-11 <lambda-school-loaner-11@Lambda-School-Loaner-11s-MacBook-Pro.local>
2018-08-31 09:48:08 -0700

    setting up pan gesture

--- Sprint 6 Challenge/Sprint 6 Challenge/Controller/ViewController.swift
+++ Sprint 6 Challenge/Sprint 6 Challenge/Controller/ViewController.swift
@@ -10,16 +10,54 @@ import UIKit
 
 class ViewController: UIViewController, UIViewControllerTransitioningDelegate {
     
+    // Reference
+    
+    let animator = Animator()
+    
+    // Transition object
+    
+    private var interactionController: UIPercentDrivenInteractiveTransition?
+    
+    // Image references
+    
+    let lockedImage: UIImageView = {
+        let image = UIImageView()
+        image.image = UIImage(named: "Locked")
+        return image
+    }()
+    
+    let unLockedImage: UIImageView = {
+        let image = UIImageView()
+        image.image = UIImage(named: "Unlocked")
+        return image
+    }()
+    
     @IBOutlet weak var lockImage: UIImageView!
     
+    // Lock image on transition
     
     @IBAction func reset(_ sender: UIBarButtonItem) {
-        
+        transitioningDelegate = self
+        lockImage.image = lockedImage.image
     }
-
+    
+    @IBAction func panGesture(_ sender: UIPanGestureRecognizer) {
+        
+        switch sender.state {
+        case .began:
+            print("swipe began")
+        case .changed:
+            print("swiped")
+        case .ended:
+            print("swipe ended")
+            lockImage.image = unLockedImage.image
+        default:
+            break
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-    
     
     }
 }

b752bf79b7dd0a793630f2eded74879d05126f7e
Samantha Gatt <smfgatt@gmail.com>
2018-08-31 12:15:24 -0400

    Configures storyboard

---
+++ Sprint 6 Challenge/Sprint 6 Challenge/SwipeToUnlockControl.swift
@@ -0,0 +1,60 @@
+//
+//  SwipeToUnlockControl.swift
+//  Sprint 6 Challenge
+//
+//  Created by Samantha Gatt on 8/31/18.
+//  Copyright © 2018 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+class SwipeToUnlockControl: UIControl {
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        
+        
+//        setupSubviewCustomControl()
+//        print("HELLO WORLD!!!!!", oval)
+    }
+    
+    
+    // MARK: - Properties
+    
+    var oval: UIView!
+    
+    
+    // MARK: - functions
+    
+//    func setupSubviewCustomControl() {
+//        let oval = UIView()
+//        translatesAutoresizingMaskIntoConstraints = false
+//        oval.backgroundColor = UIColor.darkGray
+//        addSubview(oval)
+//
+//        print("HELLO WORLD!!!!!!! 2", self.center)
+//        let ovalCenterY = NSLayoutConstraint(item: oval, attribute: .centerY,
+//                                             relatedBy: .equal,
+//                                             toItem: self, attribute: .centerY,
+//                                             multiplier: 1,
+//                                             constant: 0.0)
+//        let ovalLeading = NSLayoutConstraint(item: oval, attribute: .leading,
+//                                             relatedBy: .equal,
+//                                             toItem: self, attribute: .leading,
+//                                             multiplier: 1,
+//                                             constant: 0.0)
+//        let ovalWidth = NSLayoutConstraint(item: oval, attribute: .width,
+//                                           relatedBy: .equal,
+//                                           toItem: nil, attribute: .notAnAttribute,
+//                                           multiplier: 1,
+//                                           constant: 40.0)
+//        let ovalHeight = NSLayoutConstraint(item: oval, attribute: .height,
+//                                            relatedBy: .equal,
+//                                            toItem: nil, attribute: .notAnAttribute,
+//                                            multiplier: 1,
+//                                            constant: 40.0)
+//        NSLayoutConstraint.activate([ovalCenterY, ovalLeading, ovalWidth, ovalHeight])
+//
+//        self.oval = oval
+//    }
+}

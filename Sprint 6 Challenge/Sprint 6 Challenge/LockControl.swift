4814f43cc0e1318a7f629e5f506f91cf2328d357
Andrew Liao <andrewoliao@gmail.com>
2018-08-31 09:54:34 -0700

    Create appearance of custom control

--- Sprint 6 Challenge/Sprint 6 Challenge/LockControl.swift
+++ Sprint 6 Challenge/Sprint 6 Challenge/LockControl.swift
@@ -10,5 +10,84 @@ import UIKit
 
 class LockControl: UIControl {
     
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        setup()
+    }
+    
+    func setup(){
+        
+        clipsToBounds = true
+        layer.cornerRadius = 16
+        layer.backgroundColor = Appearance.lightSteelBlue.cgColor
+        
+        createImageView()
+        createLockBar()
+    }
+    
+    func createImageView(){
+        let width = self.frame.width/2
+        let x = width/2
+        let y = width/2
+        
+        let frame = CGRect(x: x, y: y, width: width, height: width)
+        let imageView = UIImageView(frame: frame)
+        imageView.contentMode = .scaleAspectFit
+        imageView.image = lockedImage
+        
+        addSubview(imageView)
+    }
+    
+    func createLockBar(){
+        //create Bar
+        let width = self.frame.width - 16*2
+        let height = self.frame.height/6
+        let y = self.frame.height - height - 10
+        
+        let lockFrame = CGRect(x: 16, y: y, width: width, height: height)
+        let lockBar = UIView(frame: lockFrame)
+        lockBar.layer.backgroundColor = UIColor.white.cgColor
+        lockBar.clipsToBounds = true
+        lockBar.layer.cornerRadius = height/2
+        addSubview(lockBar)
+        
+        //create bolt of lock bar
+        
+        let radius = lockBar.frame.height - 6
+        let boltFrame = CGRect(x: 5, y: 5/2, width: radius, height: radius)
+        let bolt = UIView(frame: boltFrame)
+        bolt.clipsToBounds = true
+        bolt.layer.cornerRadius = radius/2
+        bolt.layer.backgroundColor = Appearance.midnightBlue.cgColor
+        lockBar.addSubview(bolt)
+        
+        
+    }
+    
+    //Properties
     var isLocked: Bool = true
+    private let lockedImage = UIImage(named: "Locked")
+    private let unlockedImage = UIImage(named: "Unlocked")
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
 }

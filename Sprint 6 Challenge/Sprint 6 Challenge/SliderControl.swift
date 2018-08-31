8edf07eb1ec37ed2ab2986609150e4cb97d97ed9
Conner Alegre <cga0@pm.me>
2018-08-31 11:18:00 -0600

    SliderControl base view

--- Sprint 6 Challenge/Sprint 6 Challenge/SliderControl.swift
+++ Sprint 6 Challenge/Sprint 6 Challenge/SliderControl.swift
@@ -8,4 +8,27 @@
 
 import UIKit
 
+class SliderControl: UIControl {
+  
+  public override init(frame: CGRect) {
+    super.init(frame: frame)
+    setup()
+  }
+  
+  required init?(coder aCoder: NSCoder) {
+    super.init(coder: aCoder)
+    setup()
+  }
 
+  private let sliderControl = UILabel(frame: CGRect(x: 0, y: 0, width: 40, height: 41))
+  private var progress = 0.0
+  
+  func setup() {
+    self.backgroundColor = UIColor.red
+    
+    sliderControl.textAlignment = .center
+    sliderControl.backgroundColor = .black
+    
+    self.addSubview(sliderControl)
+  }
+}

aa27224f4bef3ab5c649bddbc6345ec46fbaed69
Conner Alegre <cga0@pm.me>
2018-08-31 21:48:55 -0600

    slider complete with progress tracking

--- Sprint 6 Challenge/Sprint 6 Challenge/SliderControl.swift
+++ Sprint 6 Challenge/Sprint 6 Challenge/SliderControl.swift
@@ -9,46 +9,65 @@
 import UIKit
 
 class SliderControl: UIControl {
-  
+
   public override init(frame: CGRect) {
     super.init(frame: frame)
     setup()
   }
-  
+
   required init?(coder aCoder: NSCoder) {
     super.init(coder: aCoder)
     setup()
   }
-  
-  private let sliderControl = UIView(frame: CGRect(x: 0, y: 0, width: 40, height: 41))
+
+  private let sliderControl = UIView(frame: CGRect(x: 0, y: 0, width: 65, height: 41))
   private var progress = 0.0
-  
+  private var shouldMove = false
+
   private func setup() {
     self.backgroundColor = UIColor.lightGray
-    
-//    sliderControl.textAlignment = .center
+    sliderControl.clipsToBounds = true
     sliderControl.backgroundColor = .black
-    
-    
+    sliderControl.layer.masksToBounds = true
+
     let panGesture = UIPanGestureRecognizer(target: self, action: #selector(self.pan(_:)))
     self.addGestureRecognizer(panGesture)
+    self.setNeedsLayout()
     self.addSubview(sliderControl)
-
   }
-  
+
   @objc func pan(_ sender: UIPanGestureRecognizer) {
     let x = sender.location(in: self).x
-    print(x)
-    
+    if x <= 0 || x > self.bounds.size.width - self.sliderControl.bounds.size.width {
+      return
+    }
+
     switch sender.state {
     case .began:
+      shouldMove = x < sliderControl.bounds.size.width + 10
       self.sendActions(for: .editingDidBegin)
     case .changed:
-      sliderControl.frame.origin.x = x
-      self.sendActions(for: .valueChanged)
-    default:
-      break
+      if shouldMove {
+        sliderControl.frame.origin.x = x
+        progress = (Double(min(x / self.bounds.size.width, 1)) + 0.1289) * 100
+        self.sendActions(for: .valueChanged)
+      }
+    case .ended: fallthrough
+    case .cancelled:
+      if progress > 65 && sender.velocity(in: self).x > -1.0 {
+        UIView.animate(withDuration: 0.5) {
+          self.layoutIfNeeded()
+          self.sliderControl.frame.origin.x = self.bounds.size.width - self.sliderControl.bounds.size.width
+        }
+      } else {
+        UIView.animate(withDuration: 0.5) {
+          self.layoutIfNeeded()
+          self.sliderControl.frame.origin.x = 0
+          self.progress = 0.0
+        }
+      }
+    default: break
     }
   }
-  
+
 }

76039d968eec49e8af84afae253670096ad1456f
Farhan Farooqui <farhan@farooqui.net>
2018-10-05 12:39:14 -0400

    lock image added inside container view

--- Sprint 6 Challenge/Sprint 6 Challenge/ViewController.swift
+++ Sprint 6 Challenge/Sprint 6 Challenge/ViewController.swift
@@ -11,28 +11,44 @@ import UIKit
 class ViewController: UIViewController {
     
     // MARK :- Properties
-    
-    let lockImage = UIImage(named: "Locked")!
+
+    let lockedImage = UIImage(named: "Locked")!
     let unlockedImage = UIImage(named: "Unlocked")!
     
     var isUnlocked: Bool = false
     
     @IBOutlet weak var resetButton: UIBarButtonItem!
-    @IBOutlet weak var lockImageView: UIImageView!
+    
     
     // MARK :- Functions
 
     override func viewDidLoad() {
         super.viewDidLoad()
        
-        
+        setupLockScreen()
         
     }
     
     private func setupLockScreen(){
         
+        let lockImageView = UIImageView()
+        lockImageView.image = lockedImage
         
-        imageview
+        let containerViewFrame = CGRect(x: 0, y: 0, width: 350, height: 350.0)
+        let containerView = UIView(frame: containerViewFrame)
+        containerView.center.x = view.center.x
+        containerView.center.y = view.center.y
+        containerView.backgroundColor = .gray
+        
+//        lockImageView.image = lockImage
+//        lockImageView.contentMode = .scaleAspectFit
+        let imageViewFrame = CGRect(x: 0, y: 0, width: lockedImage.size.width, height: lockedImage.size.height)
+        lockImageView.frame = imageViewFrame
+        lockImageView.center.x = containerView.frame.width / 2
+        lockImageView.layoutIfNeeded()
+        
+        containerView.addSubview(lockImageView)
+        view.addSubview(containerView)
         
     }
 

156727039449a8ce40d798e521a805d873cb4f29
Scott Bennett <stt.btt@gmail.com>
2018-10-06 19:27:41 -0400

    Images setup

--- Sprint 6 Challenge/Sprint 6 Challenge/View Controllers/UnlockedViewController.swift
+++ Sprint 6 Challenge/Sprint 6 Challenge/View Controllers/UnlockedViewController.swift
@@ -1,27 +1,27 @@
 //
-//  ViewController.swift
+//  UnlockedViewController.swift
 //  Sprint 6 Challenge
 //
-//  Created by Spencer Curtis on 8/29/18.
+//  Created by Scott Bennett on 10/6/18.
 //  Copyright © 2018 Lambda School. All rights reserved.
 //
 
 import UIKit
 
-class ViewController: UIViewController {
+class UnlockedViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
         
-        
-        
+        view.layoutIfNeeded()
+
         let lockView = UIView()
         lockView.translatesAutoresizingMaskIntoConstraints = false
         lockView.backgroundColor = .lightGray
         lockView.layer.cornerRadius = 50
-
+        
         view.addSubview(lockView)
-
+        
         let lockCenterYConstraint = NSLayoutConstraint(item: lockView,
                                                        attribute: .centerY,
                                                        relatedBy: .equal,
@@ -29,7 +29,7 @@ class ViewController: UIViewController {
                                                        attribute: .centerY,
                                                        multiplier: 1.0,
                                                        constant: 0)
-
+        
         let lockCenterXConstraint = NSLayoutConstraint(item: lockView,
                                                        attribute: .centerX,
                                                        relatedBy: .equal,
@@ -37,7 +37,7 @@ class ViewController: UIViewController {
                                                        attribute: .centerX,
                                                        multiplier: 1.0,
                                                        constant: 0)
-
+        
         let lockWidthConstraint = NSLayoutConstraint(item: lockView,
                                                      attribute: .width,
                                                      relatedBy: .equal,
@@ -45,7 +45,7 @@ class ViewController: UIViewController {
                                                      attribute: .notAnAttribute,
                                                      multiplier: 1.0,
                                                      constant: 300)
-
+        
         let lockHeightConstraint = NSLayoutConstraint(item: lockView,
                                                       attribute: .height,
                                                       relatedBy: .equal,
@@ -53,17 +53,17 @@ class ViewController: UIViewController {
                                                       attribute: .notAnAttribute,
                                                       multiplier: 1.0,
                                                       constant: 300)
-
+        
         NSLayoutConstraint.activate([lockCenterYConstraint, lockCenterXConstraint, lockWidthConstraint, lockHeightConstraint])
-
-
+        
+        
         let slideView = UIView()
         slideView.translatesAutoresizingMaskIntoConstraints = false
         slideView.backgroundColor = .gray
         slideView.layer.cornerRadius = 25
-
+        
         view.addSubview(slideView)
-
+        
         let slideBottomConstraint = NSLayoutConstraint(item: slideView,
                                                        attribute: .bottom,
                                                        relatedBy: .equal,
@@ -71,7 +71,7 @@ class ViewController: UIViewController {
                                                        attribute: .bottom,
                                                        multiplier: 1.0,
                                                        constant: -10)
-
+        
         let slideCenterXConstraint = NSLayoutConstraint(item: slideView,
                                                         attribute: .centerX,
                                                         relatedBy: .equal,
@@ -79,7 +79,7 @@ class ViewController: UIViewController {
                                                         attribute: .centerX,
                                                         multiplier: 1.0,
                                                         constant: 0)
-
+        
         let slideWidthConstraint = NSLayoutConstraint(item: slideView,
                                                       attribute: .width,
                                                       relatedBy: .equal,
@@ -87,7 +87,7 @@ class ViewController: UIViewController {
                                                       attribute: .notAnAttribute,
                                                       multiplier: 1.0,
                                                       constant: 250)
-
+        
         let slideHeightConstraint = NSLayoutConstraint(item: slideView,
                                                        attribute: .height,
                                                        relatedBy: .equal,
@@ -95,18 +95,18 @@ class ViewController: UIViewController {
                                                        attribute: .notAnAttribute,
                                                        multiplier: 1.0,
                                                        constant: 50)
-
+        
         NSLayoutConstraint.activate([slideBottomConstraint, slideCenterXConstraint, slideWidthConstraint, slideHeightConstraint])
-
-
+        
+        
         let buttonView = UIView()
         
         buttonView.translatesAutoresizingMaskIntoConstraints = false
         buttonView.backgroundColor = .black
         buttonView.layer.cornerRadius = 20
-
+        
         view.addSubview(buttonView)
-
+        
         let buttonBottomConstraint = NSLayoutConstraint(item: buttonView,
                                                         attribute: .bottom,
                                                         relatedBy: .equal,
@@ -114,15 +114,15 @@ class ViewController: UIViewController {
                                                         attribute: .bottom,
                                                         multiplier: 1.0,
                                                         constant: -5)
-
+        
         let buttonCenterXConstraint = NSLayoutConstraint(item: buttonView,
                                                          attribute: .centerX,
                                                          relatedBy: .equal,
                                                          toItem: view,
                                                          attribute: .centerX,
                                                          multiplier: 1.0,
-                                                         constant: -100)
-
+                                                         constant: 100)
+        
         let buttonWidthConstraint = NSLayoutConstraint(item: buttonView,
                                                        attribute: .width,
                                                        relatedBy: .equal,
@@ -130,7 +130,7 @@ class ViewController: UIViewController {
                                                        attribute: .notAnAttribute,
                                                        multiplier: 1.0,
                                                        constant: 40)
-
+        
         let buttonHeightConstraint = NSLayoutConstraint(item: buttonView,
                                                         attribute: .height,
                                                         relatedBy: .equal,
@@ -138,69 +138,59 @@ class ViewController: UIViewController {
                                                         attribute: .notAnAttribute,
                                                         multiplier: 1.0,
                                                         constant: 40)
-
+        
         NSLayoutConstraint.activate([buttonBottomConstraint, buttonCenterXConstraint, buttonWidthConstraint, buttonHeightConstraint])
-       
+        
         let lockedImageView = UIImageView()
         lockedImageView.translatesAutoresizingMaskIntoConstraints = false
-        let image = UIImage(named: "Locked")
+        let image = UIImage(named: "Unlocked")
         lockedImageView.image = image
         view.addSubview(lockedImageView)
         
         let lockedImageCenterYConstraint = NSLayoutConstraint(item: lockedImageView,
-                                                       attribute: .centerY,
-                                                       relatedBy: .equal,
-                                                       toItem: view,
-                                                       attribute: .centerY,
-                                                       multiplier: 1.0,
-                                                       constant: -25)
-
+                                                              attribute: .centerY,
+                                                              relatedBy: .equal,
+                                                              toItem: view,
+                                                              attribute: .centerY,
+                                                              multiplier: 1.0,
+                                                              constant: -25)
+        
         let lockedImageCenterXConstraint = NSLayoutConstraint(item: lockedImageView,
-                                                       attribute: .centerX,
-                                                       relatedBy: .equal,
-                                                       toItem: view,
-                                                       attribute: .centerX,
-                                                       multiplier: 1.0,
-                                                       constant: 0)
-
+                                                              attribute: .centerX,
+                                                              relatedBy: .equal,
+                                                              toItem: view,
+                                                              attribute: .centerX,
+                                                              multiplier: 1.0,
+                                                              constant: 0)
+        
         let lockedImageWidthConstraint = NSLayoutConstraint(item: lockedImageView,
-                                                     attribute: .width,
-                                                     relatedBy: .equal,
-                                                     toItem: nil,
-                                                     attribute: .notAnAttribute,
-                                                     multiplier: 1.0,
-                                                     constant: 125)
-
+                                                            attribute: .width,
+                                                            relatedBy: .equal,
+                                                            toItem: nil,
+                                                            attribute: .notAnAttribute,
+                                                            multiplier: 1.0,
+                                                            constant: 125)
+        
         let lockedImageHeightConstraint = NSLayoutConstraint(item: lockedImageView,
-                                                      attribute: .height,
-                                                      relatedBy: .equal,
-                                                      toItem: nil,
-                                                      attribute: .notAnAttribute,
-                                                      multiplier: 1.0,
-                                                      constant: 150)
-
+                                                             attribute: .height,
+                                                             relatedBy: .equal,
+                                                             toItem: nil,
+                                                             attribute: .notAnAttribute,
+                                                             multiplier: 1.0,
+                                                             constant: 150)
+        
         NSLayoutConstraint.activate([lockedImageCenterYConstraint, lockedImageCenterXConstraint, lockedImageWidthConstraint,lockedImageHeightConstraint])
-        
-//        lockedImage.updateConstraints()
-        
     }
     
-    
-    
-    @IBAction func reset(_ sender: Any) {
-        
-        UIView.animate(withDuration: 1.0) {
-            
-        
-            
-        }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
     }
-    
-    
-
-    
-
-
+    */
 
 }
-

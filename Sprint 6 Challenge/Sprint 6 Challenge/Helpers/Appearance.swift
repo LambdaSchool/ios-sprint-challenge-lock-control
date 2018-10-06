ba2639d4effac84563508b53958d97f17b2b30d0
Jason Modisett <jason@Jasons-MacBook-Pro.local>
2018-10-06 17:10:13 -0500

    Added lock control logic + code cleanup

--- Sprint 6 Challenge/Sprint 6 Challenge/Helpers/Appearance.swift
+++ Sprint 6 Challenge/Sprint 6 Challenge/Helpers/Appearance.swift
@@ -10,10 +10,10 @@ import UIKit
 
 enum Appearance {
     
-    static var lambdaRed = UIColor(red: 212.0/255.0, green: 87.0/255.0, blue: 80.0/255.0, alpha: 1.0)
+    static var red = UIColor(red: 232.0/255.0, green: 87.0/255.0, blue: 80.0/255.0, alpha: 1.0)
     
     static func setup() {
-        UINavigationBar.appearance().barTintColor = lambdaRed
+        UINavigationBar.appearance().barTintColor = red
         
         let attr = [NSAttributedString.Key.foregroundColor: UIColor.white]
         

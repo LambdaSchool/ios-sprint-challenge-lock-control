b62cad0fee54db8f88026fe22cc8f8699686f16e
Lotanna <sugabelly@gmail.com>
2018-11-16 13:44:23 -0500

    kinda working

---
+++ Sprint 6 Challenge/Sprint 6 Challenge/LockViewController.swift
@@ -0,0 +1,39 @@
+//
+//  LockViewController.swift
+//  Sprint 6 Challenge
+//
+//  Created by Lotanna Igwe-Odunze on 11/16/18.
+//  Copyright © 2018 Lambda School. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class LockViewController: UIViewController, UnlockDelegate {
+   
+    //Slide Button Delegate
+    func buttonStatus(status: String, sender: SlideUnlock) {
+        print(status)
+    }
+    
+    @IBOutlet weak var unlocker: SlideUnlock!
+    
+    @IBOutlet weak var padlock: UIImageView!
+    
+    @IBAction func resetButton(_ sender: Any) {
+        self.unlocker.reset() //Resets the slider
+    }
+    
+    
+    //Set delegate for slide to unlock
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.unlocker.delegate = self
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+    
+}

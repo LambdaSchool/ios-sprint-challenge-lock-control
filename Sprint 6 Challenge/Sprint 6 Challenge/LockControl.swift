d9cd531b3ef037b29be4dc11174d69648c937d84
Jerrick Warren <Jerrick.Warren@gmail.com>
2018-11-16 10:12:51 -0800

    finished views creation but views don't look proper.  Scaling is off.  Colors are nice tho :)

--- Sprint 6 Challenge/Sprint 6 Challenge/LockControl.swift
+++ Sprint 6 Challenge/Sprint 6 Challenge/LockControl.swift
@@ -30,11 +30,23 @@ class LockControl: UIControl {
     
     var lockImage = UIImageView()
     var ball = UIView()
+    var ballFrame = UIView()
     
     private func setupControl() {
         // make each item a layer , like in photoshop
-        // image layer(lock),
         
+        // ball layer (use corner radius to make round
+        ball.frame = CGRect(x: 0, y: 0, width: self.bounds.width / 2, height: self.bounds.height / 2)
+        ball.backgroundColor = Appearance.michiganBlue
+        ball.layer.cornerRadius = self.bounds.height / 4
+        addSubview(ball)
+        
+        // ball frame
+        ballFrame.frame = CGRect(x: 20, y: 20, width: self.bounds.width - 10, height: self.bounds.height - 50)
+        ballFrame.backgroundColor = Appearance.michiganMaize
+        addSubview(ballFrame)
+        
+        // image layer(lock),
         lockImage.image = UIImage(named: "Locked")
         lockImage.frame = CGRect(x: 0, y: 0, width: self.bounds.width, height: self.bounds.height)
         addSubview(lockImage)

cb1f2cb084fdcbfc522d03a577605de57062e267
Frulwinn <frulwinn@gmail.com>
2019-01-11 08:04:27 -0800

    initial commit

--- Sprint 6 Challenge/Sprint 6 Challenge/ViewController.swift
+++ Sprint 6 Challenge/Sprint 6 Challenge/ViewController.swift
@@ -1,11 +1,3 @@
-//
-//  ViewController.swift
-//  Sprint 6 Challenge
-//
-//  Created by Spencer Curtis on 8/29/18.
-//  Copyright © 2018 Lambda School. All rights reserved.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
@@ -14,12 +6,5 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
 

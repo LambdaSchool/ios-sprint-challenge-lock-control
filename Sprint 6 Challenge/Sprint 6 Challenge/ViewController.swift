b19ab3e7d48830f8465c67e27a6d402af70752ce
AustinBCole <austin.cole.chileno@gmail.com>
2019-01-11 12:03:18 -0700

    Added thumb slider and basic UI without theme.

--- Sprint 6 Challenge/Sprint 6 Challenge/ViewController.swift
+++ Sprint 6 Challenge/Sprint 6 Challenge/ViewController.swift
@@ -9,14 +9,32 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    static let shared = ViewController()
+    
+    @IBOutlet weak var thumbSliderContainer: UIView!
+    @IBOutlet weak var containerView: UIView!
+    @IBOutlet weak var lockedUnlockedImage: UIImageView!
+    
+    var boolean = false {
+        didSet {
+            if boolean == true {
+                lockedUnlockedImage.image = UIImage(named: "Unlocked")
+            }
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        containerView.layer.cornerRadius = 25
+        thumbSliderContainer.layer.cornerRadius = 15
+        lockedUnlockedImage.image = UIImage(named: "Locked")
+
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
+
         // Dispose of any resources that can be recreated.
     }
 

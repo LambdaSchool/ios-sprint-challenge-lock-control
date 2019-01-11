27a8d6c79abbd22e88800ed504a0be67908ab106
jkaunert <44586402+jkaunert@users.noreply.github.com>
2019-01-11 11:59:43 -0600

    implements app skeleton, AppearanceHelper for theming, appearance() setup

---
+++ Sprint 6 Challenge/Sprint 6 Challenge/AppearanceHelper.swift
@@ -0,0 +1,24 @@
+import UIKit
+
+struct AppearanceHelper {
+    
+    static var lambdaRed = UIColor(red: 212.0/255.0, green: 87.0/255.0, blue: 80.0/255.0, alpha: 1.0)
+    
+    static func setAppearance() {
+        UINavigationBar.appearance().barTintColor = lambdaRed
+        UIBarButtonItem.appearance().tintColor = .black
+        
+        let textAttributes = [NSAttributedString.Key.foregroundColor: UIColor.white]
+        
+        UINavigationBar.appearance().largeTitleTextAttributes = textAttributes
+        UINavigationBar.appearance().titleTextAttributes = textAttributes
+    }
+    
+    static func style(view: UIView) {
+        view.layer.cornerRadius = 30
+    }
+    
+    static func style(slider: UISlider) {
+        slider.layer.cornerRadius = 20
+    }
+}
--- Sprint 6 Challenge/Sprint 6 Challenge/ViewController.swift
+++ Sprint 6 Challenge/Sprint 6 Challenge/ViewController.swift
@@ -13,6 +13,9 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        AppearanceHelper.style(view: controlContainerView)
+        AppearanceHelper.style(slider: lockSlider)
+        lockSlider.setValue(0.05, animated: false)
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +23,15 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBOutlet weak var controlContainerView: UIView!
+    
+    @IBOutlet weak var lockIMageView: UIImageView!
+    
+    @IBOutlet weak var lockSlider: UISlider!
+    
+    
+    @IBAction func controlLock(_ sender: Any) {
+        
+    }
 }
 

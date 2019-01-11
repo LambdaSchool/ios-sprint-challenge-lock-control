48e0b47053dab656e305a7009d9bb0fded43acab
jkaunert <44586402+jkaunert@users.noreply.github.com>
2019-01-11 14:45:53 -0600

    implements functioning  MVP

--- Sprint 6 Challenge/Sprint 6 Challenge/ViewController.swift
+++ Sprint 6 Challenge/Sprint 6 Challenge/ViewController.swift
@@ -1,11 +1,3 @@
-//
-//  ViewController.swift
-//  Sprint 6 Challenge
-//
-//  Created by Spencer Curtis on 8/29/18.
-//  Copyright © 2018 Lambda School. All rights reserved.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
@@ -16,12 +8,11 @@ class ViewController: UIViewController {
         AppearanceHelper.style(view: controlContainerView)
         AppearanceHelper.style(slider: lockSlider)
         lockSlider.setValue(0.05, animated: false)
+        if isUnlocked == false {
+        lockSlider.addTarget(self, action: #selector(onSliderValChanged(slider:event:)), for: .valueChanged)
+        }
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+    
 
     @IBOutlet weak var controlContainerView: UIView!
     
@@ -33,5 +24,59 @@ class ViewController: UIViewController {
     @IBAction func controlLock(_ sender: Any) {
         
     }
+    var isUnlocked: Bool = false
+    
+    
+    
+    @IBAction func resetLock(_ sender: Any) {
+        lockSlider.setValue(0.05, animated: true)
+        if self.lockSlider.value < 0.95 {
+            lockIMageView.image = UIImage(named: "Locked")
+            navigationItem.rightBarButtonItem?.title = ""
+            isUnlocked = false
+            lockSlider.isUserInteractionEnabled = true
+        }
+    }
+    
+    @objc func onSliderValChanged(slider: UISlider, event: UIEvent) {
+        if let touchEvent = event.allTouches?.first {
+            switch touchEvent.phase {
+            case .began:
+                //print("drag begun")
+                if self.lockSlider.value > 0.95 {
+                    isUnlocked = true
+
+                    lockIMageView.image = UIImage(named: "Unlocked")
+                    navigationItem.rightBarButtonItem?.title = "Reset"
+                    lockSlider.isUserInteractionEnabled = false
+                    lockSlider.setValue(0.96, animated: false)
+                }
+            case .moved:
+                //print("slider moved")
+                if self.lockSlider.value > 0.95 {
+                    isUnlocked = true
+                    
+                    lockIMageView.image = UIImage(named: "Unlocked")
+                    navigationItem.rightBarButtonItem?.title = "Reset"
+                    lockSlider.isUserInteractionEnabled = false
+                    lockSlider.setValue(0.96, animated: false)
+                }
+            case .ended:
+                //print("user interaction ended")
+                if self.lockSlider.value < 0.95 {
+                    lockSlider.setValue(0.05, animated: true)
+                    lockIMageView.image = UIImage(named: "Locked")
+                    navigationItem.rightBarButtonItem?.title = ""
+                    lockSlider.isUserInteractionEnabled = true
+                }else {
+                    lockSlider.setValue(0.96, animated: false)
+                }
+            default:
+                break
+            }
+        }
+    }
+
+    
 }
 

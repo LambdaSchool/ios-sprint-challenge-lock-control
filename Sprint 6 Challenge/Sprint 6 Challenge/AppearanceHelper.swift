75a868b9cf418507253b877cf207fdf51862aace
Julian A. Fordyce <julian.a.fordyce@gmail.com>
2019-01-11 11:24:19 -0500

    add color theme to navigation area

--- Sprint 6 Challenge/Sprint 6 Challenge/AppearanceHelper.swift
+++ Sprint 6 Challenge/Sprint 6 Challenge/AppearanceHelper.swift
@@ -6,4 +6,15 @@
 //  Copyright © 2019 Lambda School. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+enum AppearanceHelper {
+    
+    static var someBlue = UIColor(red:37.0/255.0, green:74.0/255.0, blue: 108.0/255.0, alpha: 1.0)
+   
+    static func setAppearance() {
+        UINavigationBar.appearance().backgroundColor = someBlue
+        UIBarButtonItem.appearance().tintColor = someBlue
+    }
+}
+

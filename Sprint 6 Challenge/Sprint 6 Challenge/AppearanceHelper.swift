ef9c2f18eb59d2b777a51f93f0752f0b24b0518f
Julian A. Fordyce <julian.a.fordyce@gmail.com>
2019-01-11 11:09:14 -0500

    create an appearance helper to hold styling for app

---
+++ Sprint 6 Challenge/Sprint 6 Challenge/AppearanceHelper.swift
@@ -0,0 +1,9 @@
+//
+//  AppearanceHelper.swift
+//  Sprint 6 Challenge
+//
+//  Created by Julian A. Fordyce on 1/11/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import Foundation

fab019f4361b840bf3b7582e0d8090ef18bcd421
Frulwinn <frulwinn@gmail.com>
2019-01-11 08:51:37 -0800

    added a colorsxcassets folder

--- Sprint 6 Challenge/Sprint 6 Challenge/AppearanceHelper.swift
+++ Sprint 6 Challenge/Sprint 6 Challenge/AppearanceHelper.swift
@@ -2,13 +2,15 @@ import UIKit
 
 enum AppearanceHelper {
     
-    static var lambdaRed = UIColor(red: 212.0/255.0, green: 87.0/255.0, blue: 80.0/255.0, alpha: 1.0)
+    static var lipstickRed = UIColor(named: "lipstickRed")
+    static var darkGray = UIColor(named: "darkGray")
     
     static func setDarkAppearance() {
-        UINavigationBar.appearance().barTintColor = lambdaRed
+        UINavigationBar.appearance().barTintColor = lipstickRed
         
         let textAttributes = [NSAttributedStringKey.foregroundColor: UIColor.white]
         
         UINavigationBar.appearance().titleTextAttributes = textAttributes as [NSAttributedStringKey: Any]
+        UIBarButtonItem.appearance().setTitleTextAttributes([NSAttributedStringKey.foregroundColor : UIColor.black,], for: .normal)
     }
 }

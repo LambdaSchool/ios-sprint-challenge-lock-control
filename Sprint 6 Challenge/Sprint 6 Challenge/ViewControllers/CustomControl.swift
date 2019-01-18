0cf573e1850cadf5b5dbfb62c9e868138700550c
IvanCaldwell <grumpy.caldwell@gmail.com>
2019-01-18 11:00:36 -0800

    Kinda Working

--- Sprint 6 Challenge/Sprint 6 Challenge/ViewControllers/CustomControl.swift
+++ Sprint 6 Challenge/Sprint 6 Challenge/ViewControllers/CustomControl.swift
@@ -10,9 +10,10 @@ import Foundation
 import UIKit
 
 class CustomControl: UIControl {
-    var value: CGFloat = 1.0
+    static var value: CGFloat = 1.0
     let slider = UIView(frame: CGRect(x: 15, y: 320, width: 280, height: 40))
-    static var thumb = UIView(frame: CGRect(x: 17, y: 320, width: 40, height: 40))
+    static var thumb = UIView(frame: CGRect(x: 0, y: 0, width: 40, height: 40))
+    static var thumbEnable = true
     
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
@@ -26,11 +27,85 @@ class CustomControl: UIControl {
         slider.layer.cornerRadius = slider.frame.height / 2
         CustomControl.thumb.backgroundColor = UIColor.black
         CustomControl.thumb.layer.cornerRadius = CustomControl.thumb.frame.height / 2
-        addSubview(slider)
-        addSubview(CustomControl.thumb)
+        self.addSubview(slider)
+        slider.addSubview(CustomControl.thumb)
         CustomControl.thumb.isUserInteractionEnabled = false
+        slider.isUserInteractionEnabled = false
+        print("Hello Appearance Function")
     }
     
+    override func beginTracking(_ touch: UITouch, with event: UIEvent?) -> Bool {
+        let touchPoint = touch.location(in: CustomControl.thumb)
+        if slider.bounds.contains(touchPoint) {
+            print("Began Tracking\nX:\(touchPoint.x)\nY:\(touchPoint.y)\n")
+            updateValue(at: touch)
+            sendActions(for: [.touchDown, .valueChanged])
+            return true
+        }
+        return false
+    }
     
+    override func continueTracking(_ touch: UITouch, with event: UIEvent?) -> Bool {
+        let touchPoint = touch.location(in: CustomControl.thumb)
+        if slider.bounds.contains(touchPoint) {
+            updateValue(at: touch)
+            sendActions(for: [.touchDragInside, .valueChanged])
+        } else {
+            print("The touch went outside the bounds of the view")
+            print("X:\(touchPoint.x)\nY:\(touchPoint.y)\n")
+        }
+        return true
+    }
     
+    override func endTracking(_ touch: UITouch?, with event: UIEvent?) {
+        defer { super.endTracking(touch, with: event) }
+        guard let touch = touch else { return }
+        let touchPoint = touch.location(in: CustomControl.thumb)
+        print("Ended Tracking")
+        print("X:\(touchPoint.x)\nY:\(touchPoint.y)\n")
+        if slider.bounds.contains(touchPoint) {
+            sendActions(for: [.touchUpInside, .valueChanged])
+            if touchPoint.x <= 192 {
+                UIView.animate(withDuration: 1.0) {
+                    CustomControl.thumb.frame = CGRect(x: 0, y: 0, width: 40, height: 40)
+                    CustomControl.value = 0
+                }
+            } else {
+                CustomControl.thumb.frame = CGRect(x: 240, y: 0, width: 40, height: 40)
+                CustomControl.value = 240
+            }
+            updateThumbValue(at: touch)
+        } else {
+            sendActions(for: [.touchUpOutside])
+        }
+        
+    }
+    
+    override func cancelTracking(with event: UIEvent?) {
+        super.cancelTracking(with: event)
+        sendActions(for: [.touchCancel])
+    }
+    
+    func updateValue(at touch: UITouch) {
+        let touchPoint = touch.location(in: CustomControl.thumb)
+        if slider.bounds.contains(touchPoint) {
+            if touchPoint.x < 240 && touchPoint.x > 20 {
+                print(CustomControl.value)
+                sendActions(for: [.valueChanged])
+                CustomControl.thumb.center.x = touchPoint.x
+            }
+        }
+    }
+    
+    func updateThumbValue(at touch: UITouch) {
+        let touchPoint = touch.location(in: CustomControl.thumb)
+        if slider.bounds.contains(touchPoint) {
+            if touchPoint.x < 240 && touchPoint.x > 20 {
+                CustomControl.value = touchPoint.x
+                print(CustomControl.value)
+                sendActions(for: [.valueChanged])
+                //CustomControl.thumb.center.x = touchPoint.x
+            }
+        }
+    }
 }
--- Sprint 6 Challenge/Sprint 6 Challenge/ViewControllers/ViewController.swift
+++ Sprint 6 Challenge/Sprint 6 Challenge/ViewControllers/ViewController.swift
@@ -20,15 +20,27 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         resetButton.title = ""
         resetButton.isEnabled = false
-        
     }
     
     
     @IBAction func resetButtonTapped(_ sender: Any) {
-        
+        self.padlockImage.image = UIImage(named: "Locked")
+        resetButton.title = ""
+        resetButton.isEnabled = false
+        CustomControl.thumb.frame = CGRect(x: 0, y: 0, width: 40, height: 40)
     }
     
     @IBAction func updateImage(_ sender: CustomControl) {
-        
+        if !resetButton.isEnabled {
+            let number = CustomControl.value
+            if number >= 192 {
+                print("Unlocked at value: \(number)")
+                self.padlockImage.image = UIImage(named: "Unlocked")
+                UIView.animate(withDuration: 3.0) {
+                    self.resetButton.title = "Reset"
+                    self.resetButton.isEnabled = true
+                }
+            }
+        }
     }
 }

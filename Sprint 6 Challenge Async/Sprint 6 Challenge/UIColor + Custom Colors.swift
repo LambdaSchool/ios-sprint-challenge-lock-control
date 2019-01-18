1d896a920d8f4aaa6a51f8fbc684cc60a4b675a7
Audrey Welch <audreywelch@Audreys-MacBook-Pro.local>
2019-01-18 10:34:24 -0600

    User Interface Built

---
+++ Sprint 6 Challenge Async/Sprint 6 Challenge/UIColor + Custom Colors.swift
@@ -0,0 +1,14 @@
+
+import UIKit
+
+extension UIColor {
+    
+    static let almond = UIColor(red: 0.93, green: 0.88, blue: 0.80, alpha: 1.0)
+    
+    static let pastelBlue = UIColor(red: 0.69, green: 0.82, blue: 0.83, alpha: 1.0)
+    
+    static let lavender = UIColor(red: 0.75, green: 0.52, blue: 0.59, alpha: 1.0)
+    
+    static let wenge = UIColor(red: 0.45, green: 0.34, blue: 0.32, alpha: 1.0)
+    
+}

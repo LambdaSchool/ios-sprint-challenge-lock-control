460b25585fe8ec81ef1d8da6ad8b7b19f80cfa4a
Frulwinn <frulwinn@gmail.com>
2019-01-18 09:13:01 -0800

    added variables to RangeSlider

--- Sprint 6 Challenge/Sprint 6 Challenge/RangeSlider.swift
+++ Sprint 6 Challenge/Sprint 6 Challenge/RangeSlider.swift
@@ -2,6 +2,25 @@ import UIKit
 
 class RangeSlider: UIControl {
     
+    var minimumValue: CGFloat = 0
+    var maximumValue: CGFloat = 1
+    var lowerValue: CGFloat = 0.2
+    var upperValue: CGFloat = 0.8
+    var trackColor = UIColor(white: 0.9, alpha: 1)
+    var customTintColor = UIColor(red: 0, green: 0.45, blue: 9.94, alpha: 1) {
+        didSet {
+            updateControlFrames()
+        }
+    }
     
+    private let track = RangeSliderTrack()
+    private let thumb = UIView()
+    private var previousLocation = CGPoint()
+    private var activeThumb: UIView?
     
+    private var thumbWidth: CGFloat {
+        return frame.height * 0.8
+    }
+    
+
 }

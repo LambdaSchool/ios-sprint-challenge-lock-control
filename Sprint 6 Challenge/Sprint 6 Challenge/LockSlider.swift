76538c494df21404e7887b2969e183e8864a3ab1
Jaspal Suri <44656000+JaspalSuri@users.noreply.github.com>
2019-01-18 23:45:08 -0800

    basic logic for the lock change and reset button
    - forgot to implement the actual track, I thought it wasn't needed for some reason?

--- Sprint 6 Challenge/Sprint 6 Challenge/LockSlider.swift
+++ Sprint 6 Challenge/Sprint 6 Challenge/LockSlider.swift
@@ -20,28 +20,44 @@ import UIKit
      - Change the navigation title based on the lock state
 */
     
-    
-    
     override func beginTracking(_ touch: UITouch, with event: UIEvent?) -> Bool {
+        let touchPoint = touch.location(in: self)
         sendActions(for: .touchDown)
-        return true
+        if keyThumb.bounds.contains(touchPoint) {
+            sendActions(for: .touchDown)
+            
+            return true
+        } else {
+            return false
+        }
     }
     
     override func continueTracking(_ touch: UITouch, with event: UIEvent?) -> Bool {
         let touchPoint = touch.location(in: self)
-        if bounds.contains(touchPoint) {
-            sendActions(for: .touchDragInside)
-        } else {
-            sendActions(for: .touchDragOutside)
-        }
+        
+        let locationChange = touchPoint.x - previousLocation.x
+        
+        let valueChange = (maximumValue - minimumValue) * locationChange / bounds.width
+        
+        previousLocation = touchPoint
+        
+        thumbProgress = bound(value: thumbProgress + valueChange, to: maximumValue)
+        
+        sendActions(for: .valueChanged)
+        
         return true
     }
     
     override func endTracking(_ touch: UITouch?, with event: UIEvent?) {
+        defer {
+            super.endTracking(touch, with: event)
+        }
+        
         guard let touch = touch else { return }
         let touchPoint = touch.location(in: self)
+        
         if bounds.contains(touchPoint) {
-            sendActions(for: .touchUpInside)
+            sendActions(for: [.touchUpInside, .valueChanged])
         } else {
             sendActions(for: .touchUpOutside)
         }
@@ -49,6 +65,100 @@ import UIKit
     
     override func cancelTracking(with event: UIEvent?) {
         sendActions(for: .touchCancel)
+        resetkeyThumbPosition()
+    }
+    
+    // Replace the thumb at the beginning of the track.
+    func resetkeyThumbPosition() {
+        isLocked = false
+        presentLockImage.image = lockedImage
+        thumbProgress = 0.0
+    }
+    
+    // Is the padlock locked?
+    // Is  the slider > 80% of the way there?
+    // All things padlock related
+    var isLocked: Bool = true
+    var lockedImage = UIImage(named: "Locked")
+    var unlockedImage = UIImage(named: "Unlocked")
+    // Whose is present at all times and whose value to modify
+    // Force unwrap to avoid the need for initializers
+    var presentLockImage: UIImageView!
+    
+    func changeLockState() {
+        if thumbProgress >= 0.8 {
+            presentLockImage.image = unlockedImage
+        } else {
+            presentLockImage.image = lockedImage
+        }
+    }
+    
+    
+    // Thumb positions
+    
+    func lockSetup() {
+        
+    }
+    
+    var minimumValue: CGFloat = 0
+    var maximumValue: CGFloat = 1
+    var thumbProgress: CGFloat = 0.1
+    var trackColor = UIColor(white: 0.9, alpha: 1)
+    // Mostly blue
+    var customTintColor = UIColor.green.cgColor
+    
+    // Private Constants
+    private let keyThumb = UIView()
+    private var previousLocation = CGPoint()
+    
+    // 80% of the height of the slider
+    private var thumbWidth: CGFloat {
+        return frame.height * 0.8
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        
+        setupThumb(keyThumb)
+        
+   //     updateControlFrames()
+        
+    }
+    
+    // MARK: - Utility Methods
+    private func setupThumb(_ thumb: UIView) {
+        let thumbFrame = CGRect(x: 0, y: 0, width: thumbWidth, height: thumbWidth)
+        thumb.frame = thumbFrame
+        thumb.backgroundColor = .white
+        thumb.layer.cornerRadius = thumbWidth/2
+        thumb.layer.borderColor = customTintColor
+        thumb.layer.borderWidth = 1
+        changeLockState()
+        if thumbProgress >= 0.8 {
+            thumb.isUserInteractionEnabled = false
+            addSubview(presentLockImage)
+        }
+        
+        addSubview(thumb)
+    }
+    
+    private func updateControlFrames() {
+        // No long needed since we're not using a custom track
+        // Edit, it's needed since it's not visible.
+    }
+    
+    private func thumbFrame(for value: CGFloat) -> CGRect {
+        let x = position(for: value) - thumbWidth/2
+        let y = (bounds.height - thumbWidth)/2
+        return CGRect(x: x, y: y, width: thumbWidth, height: thumbWidth)
+    }
+    
+    func position(for value: CGFloat) -> CGFloat {
+        return bounds.width * value
+    }
+    
+    private func bound(value: CGFloat, to lowerValue: CGFloat) -> CGFloat {
+        return max(value, lowerValue)
     }
     
 }

139ed2e5559051c0d4e5d4876b3506d780ac32d6
Frulwinn <frulwinn@gmail.com>
2019-02-01 08:27:23 -0800

    added setup appearance in appearance enum and set it in the appDelegate

--- Sprint 6 Challenge/Sprint 6 Challenge/Helpers/Appearance.swift
+++ Sprint 6 Challenge/Sprint 6 Challenge/Helpers/Appearance.swift
@@ -6,4 +6,14 @@ enum Appearance {
         guard let font = UIFont(name: "Championship", size: pointSize) else { fatalError("Font is nil. Check the name of the font.") }
         return UIFontMetrics(forTextStyle: .callout).scaledFont(for: font)
     }
+    
+    static func setAppearance() {
+        //navigation bar
+        UINavigationBar.appearance().barTintColor = .cheetos
+        UINavigationBar.appearance().largeTitleTextAttributes = [NSAttributedStringKey.foregroundColor: UIColor.darkOrange, NSAttributedStringKey.font: UIFont(name: "Championship", size: 30)!]
+        
+        //bar button item
+        UIBarButtonItem.appearance().tintColor = .hayYellow
+        UIBarButtonItem.appearance().setTitleTextAttributes([NSAttributedStringKey.font: UIFont(name: "Championship", size: 14)!, NSAttributedStringKey.foregroundColor: UIColor.hayYellow], for: .normal)
+    }
 }

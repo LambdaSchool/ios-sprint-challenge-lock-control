7f7cb202660591b0c6178dab94024b83ca999966
Julian A. Fordyce <julian.a.fordyce@gmail.com>
2019-02-01 11:16:15 -0500

    create new file and class to set to view for custom control

---
+++ Sprint 6 Challenge/Sprint 6 Challenge/CustomControl.swift
@@ -0,0 +1,9 @@
+//
+//  CustomControl.swift
+//  Sprint 6 Challenge
+//
+//  Created by Julian A. Fordyce on 2/1/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import Foundation

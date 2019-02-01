2a6e86e487e5cf347dbb455844b557684f1f17af
Frulwinn <frulwinn@gmail.com>
2019-02-01 09:44:53 -0800

    added thumb to customcontrol

--- Sprint 6 Challenge/Sprint 6 Challenge/Controllers/CustomControl.swift
+++ Sprint 6 Challenge/Sprint 6 Challenge/Controllers/CustomControl.swift
@@ -15,5 +15,34 @@ class CustomControl: UIControl {
     var pLocation = CGPoint()
     var isUnlocked: Bool = false
     
+    //thumb set up
+    private func setupThumb(_ thumb: UIView) {
+        let thumbFrame = CGRect(x: 0, y: 0, width: thumbWidth, height: thumbWidth)
+        thumb.frame = thumbFrame
+        thumb.backgroundColor = .cheetos
+        thumb.layer.cornerRadius = thumbWidth/2
+        thumb.isUserInteractionEnabled = false
+        
+        addSubview(thumb)
+    }
     
+    func position(for value: CGFloat) -> CGFloat {
+        return(bounds.width - thumbWidth - 12) * value
+    }
+    
+    private func thumbFrame(for value: CGFloat) -> CGRect {
+        let x = position(for: value) + 6
+        let y = (bounds.height - thumbWidth)/2
+        return CGRect(x: x, y: y, width: thumbWidth, height: thumbWidth)
+    }
+    
+    private func updateControlFrames() {
+        thumb.frame = thumbFrame(for: thumbValue)
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        setupThumb(thumb)
+        updateControlFrames()
+    }
 }

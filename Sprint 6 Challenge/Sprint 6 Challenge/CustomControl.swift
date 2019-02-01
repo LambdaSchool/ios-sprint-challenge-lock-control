17f5419bbe8ae5ffb248190170add9918f1676d1
Julian A. Fordyce <julian.a.fordyce@gmail.com>
2019-02-01 12:14:26 -0500

    create properties and set up methods for custom control

--- Sprint 6 Challenge/Sprint 6 Challenge/CustomControl.swift
+++ Sprint 6 Challenge/Sprint 6 Challenge/CustomControl.swift
@@ -10,6 +10,37 @@ import UIKit
 
 class CustomControl: UIControl {
     
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        setupBall(ballView)
+        updateFrames()
+    }
+    
+    
+    private func setupBall(_ ball: UIView) {
+        
+        
+    }
+    
+    func position(for value: CGFloat) -> CGRect {
+        
+        
+    }
+    
+    
+    
+    private func ballFrame(for vlaue:CGFloat) -> CGRect {
+        
+    }
+    
+    private func updateFrames() {
+    
+    
+    }
+    
+    func reset() {
+        
+    }
     
     
     
@@ -18,24 +49,18 @@ class CustomControl: UIControl {
     
     
     
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
+        // MARK: - Properties
+        var minimumValue: CGFloat = 0
+        var maximumValue: CGFloat = 1
+        var ballValue: CGFloat = 0.0
+        var isUnlocked: Bool = false
+        
+        private var ballView = UIView()
+        private var previousLocation = CGPoint()
+        
+        private var ballWidth: CGFloat {
+            return frame.height * 0.825
+        }
     
     
     

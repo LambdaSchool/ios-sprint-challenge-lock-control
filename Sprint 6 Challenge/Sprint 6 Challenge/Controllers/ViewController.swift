98fe8d94373c08b5261bd6f6fc72159cf0c483bb
Frulwinn <frulwinn@gmail.com>
2019-02-01 08:55:29 -0800

    added outlets to objects and created a customcontrol file

--- Sprint 6 Challenge/Sprint 6 Challenge/Controllers/ViewController.swift
+++ Sprint 6 Challenge/Sprint 6 Challenge/Controllers/ViewController.swift
@@ -1,8 +1,27 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-
+    
+    //MARK: - Properties
+    @IBOutlet weak var reset: UIBarButtonItem!
+    @IBOutlet weak var lockView: UIImageView!
+    @IBOutlet weak var customControl: CustomControl!
+    @IBOutlet weak var sliderView: UIView!
+    
+    @IBAction func reset(_ sender: Any) {
+        
+    }
+    
+    @IBAction func sliderValueChanged(_ sender: Any) {
+    }
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        navigationController?.title = "Unlock"
+        
+        view.backgroundColor = .darkOrange
+    }
 
 
 }

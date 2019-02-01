430762df959bcb7389af94b2bcbbaaaa7fc4f42f
Moses Robinson <mosesjrobinson@icloud.com>
2019-02-01 11:22:27 -0800

    have basic application functionality

--- Sprint 6 Challenge/Sprint 6 Challenge/Controller/Slider.swift
+++ Sprint 6 Challenge/Sprint 6 Challenge/Controller/Slider.swift
@@ -10,13 +10,59 @@ import UIKit
 
 class Slider: UIControl {
     
+    // MARK: - Touch Tracking
     
+    override func beginTracking(_ touch: UITouch, with event: UIEvent?) -> Bool {
+        let touchPoint = touch.location(in: self)
+        
+        if touchPoint.x > self.bounds.minX + 38 && touchPoint.x < self.bounds.maxX - 38 {
+            xPosition = touchPoint.x
+        }
+        
+        return true
+    }
     
+    override func continueTracking(_ touch: UITouch, with event: UIEvent?) -> Bool {
+        let touchPoint = touch.location(in: self)
+        
+        if bounds.contains(touchPoint) {
+            
+            if touchPoint.x > self.bounds.minX + 38 && touchPoint.x < self.bounds.maxX - 38 {
+                xPosition = touchPoint.x
+            }
+            
+            sendActions(for: [.touchDragInside, .touchDragExit, .valueChanged])
+        } else {
+            sendActions(for: [.touchDragOutside])
+        }
+        return true
+    }
     
+    override func endTracking(_ touch: UITouch?, with event: UIEvent?) {
+        defer { super.endTracking(touch, with: event) }
+        
+        guard let touch = touch else { return }
+        let touchPoint = touch.location(in: self)
+        
+        if bounds.contains(touchPoint) {
+            
+            if touchPoint.x > (self.bounds.maxX / 10) * 7 {
+            xPosition = self.bounds.maxX - 38
+            } else {
+            xPosition = self.bounds.minX + 38
+            }
+            
+            sendActions(for: [.touchUpInside, .valueChanged])
+        } else {
+            sendActions(for: [.touchUpOutside])
+        }
+    }
     
+    override func cancelTracking(with event: UIEvent?) {
+        sendActions(for: [.touchCancel])
+    }
     
+    // MARK: - properties
     
-    
-    var xPosition: CGFloat = 20
-
+    var xPosition: CGFloat = 0
 }
--- Sprint 6 Challenge/Sprint 6 Challenge/ViewController/ViewController.swift
+++ Sprint 6 Challenge/Sprint 6 Challenge/ViewController/ViewController.swift
@@ -12,12 +12,35 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        self.navigationItem.rightBarButtonItem = nil
     }
 
+    @IBAction func reset(_ sender: Any) {
+        sliderView.xPosition = sliderView.bounds.minX + 38
+        circle.center.x = sliderView.xPosition
+        updateViews()
+    }
+    
     @IBAction func locking(_ sender: Slider) {
+        circle.center.x = sender.xPosition
+        updateViews()
+    }
+    
+    private func updateViews() {
         
+        if sliderView.xPosition > (sliderView.bounds.maxX / 10) * 7 {
+            lock.isLocked = true
+            title = "Locked"
+            lock.image = UIImage(named: "Locked")
+            self.navigationItem.rightBarButtonItem = resetButton
+        } else {
+            lock.isLocked = false
+            title = "Unlocked"
+            lock.image = UIImage(named: "Unlocked")
+            self.navigationItem.rightBarButtonItem = nil
+        }
         
+        lockImage.image = lock.image
     }
 
     // MARK: - Properties
@@ -26,6 +49,8 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var circle: UIView!
     @IBOutlet weak var lockImage: UIImageView!
+    @IBOutlet var resetButton: UIBarButtonItem!
     @IBOutlet weak var lockContainer: UIView!
+    @IBOutlet weak var sliderView: Slider!
 }
 

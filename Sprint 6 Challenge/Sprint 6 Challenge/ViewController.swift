bae48ad124beb2210ac30552364c96877084bfb5
“Cameron <cameron.dunn2013@gmail.com>
2019-02-01 11:51:15 -0600

    Finalized app that works as instructions requested.

--- Sprint 6 Challenge/Sprint 6 Challenge/ViewController.swift
+++ Sprint 6 Challenge/Sprint 6 Challenge/ViewController.swift
@@ -9,10 +9,46 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var navigationBar: UINavigationItem!
+    @IBOutlet weak var slider: CustomControl!
+    @IBOutlet weak var sliderConstraint: NSLayoutConstraint!
+    @IBOutlet weak var lockImage: UIImageView!
+    var sliderStartingPosition : CGPoint = CGPoint()
+    @IBAction func sliderTouched(_ sender: CustomControl) {
+        if(sliderConstraint.constant < 226){
+            self.sliderConstraint.constant += sender.amountDragged
+        }else{
+            lockImage.image = UIImage(named: "Unlocked")
+            navigationBar.rightBarButtonItem?.title = "Reset"
+        }
+    }
+    @IBAction func touchedStopped(_ sender: Any) {
+        if(sliderConstraint.constant < 226){
+            UIView.animateKeyframes(withDuration: 0.5, delay: 0, options: [], animations: {
+                UIView.addKeyframe(withRelativeStartTime: 0, relativeDuration: 0.5, animations: {
+                    self.slider.center = self.sliderStartingPosition
+                })
+            }, completion: nil)
+            sliderConstraint.constant = 8
+            slider.amountDragged = 0
+        }
+    }
+    @IBAction func resetButton(_ sender: Any) {
+        lockImage.image = UIImage(named: "Locked")
+        UIView.animateKeyframes(withDuration: 0.5, delay: 0, options: [], animations: {
+            UIView.addKeyframe(withRelativeStartTime: 0, relativeDuration: 0.5, animations: {
+                self.slider.center = self.sliderStartingPosition
+            })
+        }, completion: nil)
+        sliderConstraint.constant = 8
+        slider.amountDragged = 0
+        navigationBar.rightBarButtonItem?.title = ""
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        navigationBar.rightBarButtonItem?.title = ""
+        sliderStartingPosition = slider.center
     }
 
     override func didReceiveMemoryWarning() {

60daef84a1b7c022b82e31e71e19c77b6f2284ef
Jaspal Suri <44656000+JaspalSuri@users.noreply.github.com>
2019-02-01 15:14:52 -0800

    update View Controller with comprehensive set of outlets and connections
    I opted to change the background of the entire view instead of making another, smaller view

--- Sprint 4 User Interface Control and Animations/Sprint 4 User Interface Control and Animations/ViewController.swift
+++ Sprint 4 User Interface Control and Animations/Sprint 4 User Interface Control and Animations/ViewController.swift
@@ -12,24 +12,43 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.backgroundView.backgroundColor = UIColor.gray
-        lockImage.image = CustomControl.lockStateImage
         
-        if CustomControl.thresholdValue >= 8 {
-            resetResetButton.tintColor = .black
+        // Main View background is gray now
+        self.backgroundView.backgroundColor = UIColor.gray
+        
+//        lockImage.image = CustomControl.lockStateImage
+//
+//        if CustomControl.thresholdValue >= 8 {
+//            resetResetButton.tintColor = .black
         }
+        
         
     }
 
+    // To change store the gray background color
     @IBOutlet var backgroundView: UIView!
     
+    // To switch between locked and unlocked
     @IBOutlet weak var lockImage: UIImageView!
     
+    // to change the image and appearance of the reset button
     @IBAction func reset(_ sender: Any) {
-        CustomControl.reset()
+        // CustomControl.reset()
     }
     
+    // to store visual properties of the reset button
     @IBOutlet weak var resetResetButton: UIBarButtonItem!
+    
+    // Custom Control Connections
+    
+    @IBOutlet weak var control: CustomControl!
+    
+    @IBAction func sliderLockKey(_ sender: Any) {
+        
+    }
+    
+    @IBOutlet weak var sliderTrack: UIView!
+    
     
 }
 

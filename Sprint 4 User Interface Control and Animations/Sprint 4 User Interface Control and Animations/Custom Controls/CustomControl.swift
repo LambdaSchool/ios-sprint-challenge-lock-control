bd9cc996c7ddcd622c4397e38fba25cc08c72e32
Jaspal Suri <44656000+JaspalSuri@users.noreply.github.com>
2019-02-01 18:07:40 -0800

    implement tracking, but it doesn't work fully yet

--- Sprint 4 User Interface Control and Animations/Sprint 4 User Interface Control and Animations/Custom Controls/CustomControl.swift
+++ Sprint 4 User Interface Control and Animations/Sprint 4 User Interface Control and Animations/Custom Controls/CustomControl.swift
@@ -8,7 +8,7 @@ import UIKit
     var lhs: CGFloat = 0.0
     var rhs: CGFloat = 0.0
     
-    var curretKeyPosition: CGFloat = 0.0
+    var currentKeyPosition: CGFloat = 0.0
     
     // Draw materialize the key
     var key = UIView()
@@ -23,7 +23,7 @@ import UIKit
         key.backgroundColor = .green
         key.layer.cornerRadius = keyDimensions/2
         key.layer.borderColor = UIColor.yellow.cgColor
-        key.layer.borderWidth = 1
+        key.layer.borderWidth = 10
         key.isUserInteractionEnabled = false
         addSubview(key)
     }
@@ -45,17 +45,16 @@ import UIKit
     static var lockStateImage: UIImage = UIImage(named: "Locked")!
     static var thresholdValue = 0
     
-    
     func changeImage() {
-        if Int(curretKeyPosition) < CustomControl.thresholdValue {
+        if Int(currentKeyPosition) < CustomControl.thresholdValue {
             CustomControl.lockStateImage = LockState.lockedImage
-        } else if Int(curretKeyPosition) >= CustomControl.thresholdValue {
+        } else if Int(currentKeyPosition) >= CustomControl.thresholdValue {
             CustomControl.lockStateImage = LockState.unlockedImage
         }
     }
     
     func updateControlFrames() {
-        key.frame = keyFrame(for: curretKeyPosition)
+        key.frame = keyFrame(for: currentKeyPosition)
     }
     
     override func layoutSubviews() {
@@ -65,11 +64,71 @@ import UIKit
     }
     
     func reset() {
-        curretKeyPosition = 0.0
+        currentKeyPosition = 0.0
         updateControlFrames()
         lockState = true
     }
     
+    override func beginTracking(_ touch: UITouch, with event: UIEvent?) -> Bool {
+        
+        keyPosition = touch.location(in: self)
+        
+        if key.frame.contains(keyPosition) {
+            sendActions(for: [.touchDown, .valueChanged])
+        } else {
+            return false
+        }
+        
+        return true
+    }
     
+    override func continueTracking(_ touch: UITouch, with event: UIEvent?) -> Bool {
+        
+        let position = touch.location(in: self)
+        let keyPositionDelta = position.x - keyPosition.x
+        let positionValueDelta = (rhs - lhs) * keyPositionDelta / bounds.width
+        
+        keyPosition = position
+        
+        currentKeyPosition = min(max((currentKeyPosition + positionValueDelta), lhs), rhs)
+        
+        updateControlFrames()
+        
+        return true
+    }
+    
+    override func endTracking(_ touch: UITouch?, with event: UIEvent?) {
+        
+        defer { super.endTracking(touch, with: event) }
+        
+        guard let touch = touch else { return }
+        keyPosition = touch.location(in: self)
+        
+        if currentKeyPosition > 0.8 {
+            
+            currentKeyPosition = 1
+            
+            lockState = false
+            
+            sendActions(for: [.touchUpInside, .valueChanged])
+            
+            UIView.animate(withDuration: 1.5) {
+                self.key.frame = self.keyFrame(for: 1.0)
+            }
+        
+        } else {
+            
+            currentKeyPosition = 0
+            
+            lockState = true
+            
+            sendActions(for: [.touchUpInside, .valueChanged])
+            
+            UIView.animate(withDuration: 1.5) {
+                self.key.frame = self.keyFrame(for: 1.0)
+            }
+        }
+        updateControlFrames()
+    }
     
 }

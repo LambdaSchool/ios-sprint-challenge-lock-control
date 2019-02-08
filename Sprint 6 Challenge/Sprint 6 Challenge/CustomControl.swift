3b70c6c52c309c8daeefe7f94dc8cd0ce839a674
Paul Yi <yipaulc@gmail.com>
2019-02-08 09:39:52 -0800

    Create NSLayoutConstraints for imageView

--- Sprint 6 Challenge/Sprint 6 Challenge/CustomControl.swift
+++ Sprint 6 Challenge/Sprint 6 Challenge/CustomControl.swift
@@ -9,6 +9,69 @@
 import UIKit
 
 class CustomControl: UIControl {
+    var isUnlocked = false
+    
+    // Properties for the views in the control
+    var circleView: UIView!
+    var imageView: UIImageView!
+    
+    // This method tells Auto Layout how big your custom control should be
+    override var intrinsicContentSize: CGSize {
+        return CGSize(width: 280, height: 280)
+    }
+    
+    required init?(coder aCoder: NSCoder) {
+        super.init(coder: aCoder)
+        
+        setup()
+    }
+    
+    private func setup() {
+        
+        clipsToBounds = true
+        self.layer.cornerRadius = 50
+        
+        imageView = UIImageView()
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.image = #imageLiteral(resourceName: "Locked")
+        imageView.contentMode = .scaleAspectFit
+        self.addSubview(imageView)
+        
+        let imageWidthConstraint = NSLayoutConstraint(item: imageView,
+                                                      attribute: .width,
+                                                      relatedBy: .equal, toItem: nil,
+                                                      attribute: .notAnAttribute,
+                                                      multiplier: 1.0,
+                                                      constant: 150.0)
+        
+        let imageHeightConstraint = NSLayoutConstraint(item: imageView,
+                                                       attribute: .height,
+                                                       relatedBy: .equal,
+                                                       toItem: imageView,
+                                                       attribute: .width,
+                                                       multiplier: 1.0,
+                                                       constant: 0.0)
+        
+        let imageCenterXConstraint = NSLayoutConstraint(item: imageView,
+                                                        attribute: .centerX,
+                                                        relatedBy: .equal,
+                                                        toItem: self,
+                                                        attribute: .centerX,
+                                                        multiplier: 1.0,
+                                                        constant: 0.0)
+        
+        let imageTopConstraint = NSLayoutConstraint(item: imageView,
+                                                    attribute: .top,
+                                                    relatedBy: .equal,
+                                                    toItem: self,
+                                                    attribute: .top,
+                                                    multiplier: 1.0,
+                                                    constant: 20.0)
+        
+        NSLayoutConstraint.activate([imageWidthConstraint, imageHeightConstraint, imageCenterXConstraint, imageTopConstraint])
+    }
+    
+
    
     
 }

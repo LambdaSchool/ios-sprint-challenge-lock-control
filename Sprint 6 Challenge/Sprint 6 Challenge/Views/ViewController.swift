e628a47c297edcbcd9b7d7dc35ba05a37423cfca
victor <ruizdvictor@gmail.com>
2019-05-17 14:09:40 -0400

    added outlets and connected them to storyboard

--- Sprint 6 Challenge/Sprint 6 Challenge/Views/ViewController.swift
+++ Sprint 6 Challenge/Sprint 6 Challenge/Views/ViewController.swift
@@ -9,6 +9,14 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var resetButton: UIBarButtonItem!
+    @IBOutlet weak var sliderContainer: Slider!
+    @IBOutlet weak var container: UIView!
+    @IBOutlet weak var stackView: UIStackView!
+    @IBOutlet weak var imageView: UIImageView!
+    
+    var isUnlocked = false
+    var slider = UIView()
 
     override func viewDidLoad() {
         super.viewDidLoad()

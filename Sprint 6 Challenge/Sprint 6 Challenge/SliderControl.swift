13b480a2842bffc0dd09c28d672ab2ed0c33f1e1
hectorsvill <hectorstevenvillasano@gmail.com>
2019-05-17 08:59:56 -0700

    slider With animation

--- Sprint 6 Challenge/Sprint 6 Challenge/SliderControl.swift
+++ Sprint 6 Challenge/Sprint 6 Challenge/SliderControl.swift
@@ -9,17 +9,22 @@
 import UIKit
 
 class SliderControl: UIControl {
+	let maxOrigin = 327
+	let minOrigin = 32
 	var frameOrigin: CGRect?
+	
+	
 	
 	override func layoutSubviews() {
 		frameOrigin = frame
+		print(frame.midX)
 		backgroundColor = .red
 		clipsToBounds = true
 		layer.cornerRadius = frame.size.width / 2
 	}
 
-	// 08 is min x
-	//308 is max X
+	// 32 is min x
+	//327 is max X
 	
 	override func beginTracking(_ touch: UITouch, with event: UIEvent?) -> Bool {
 		
@@ -30,23 +35,41 @@ class SliderControl: UIControl {
 		let touch = touch.location(in: self)
 		
 		if bounds.contains(touch) && frame.midX < 280 {
-			center.x += 1
+//			center.x += 1
+			let midX = frame.midX
+			
+			if midX > 60 {
+				center.x += 3
+			} else if midX > 100 {
+				center.x += 6
+			} else {
+				center.x += 1
+			}
+			
 		}
 		
 		return true
 	}
 	
 	override func endTracking(_ touch: UITouch?, with event: UIEvent?) {
-		let touch = touch!.location(in: self)
+		//let touch = touch!.location(in: self)
 		
-		UIView.animate(withDuration: 0.2) {
-			self.frame = self.frameOrigin!
+		if frame.midX < 270 {
+			print(frame.midX)
+			UIView.animate(withDuration: 0.2) {
+				self.frame = self.frameOrigin!
+			}
+		} else {
+			UIView.animate(withDuration: 0.2, animations: {
+				self.center.x = CGFloat(self.maxOrigin)
+			}) { _ in
+				
+			}
+			
 		}
 		
+		print(frame.midX)
 		
-		
-//		print(touch)
-		print(frame)
 	}
 	
 	override func cancelTracking(with event: UIEvent?) {

35b36a2e1947e16ebc67e370a11399e71d179dcb
Lisa Sampson <lisasampson0918@gmail.com>
2019-05-17 11:18:33 -0400

    Create starter project

---
+++ Sprint 6 Challenge/Sprint 6 Challenge/Controls and UI/AppearanceHelper.swift
@@ -0,0 +1,13 @@
+//
+//  AppearanceHelper.swift
+//  Sprint 6 Challenge
+//
+//  Created by Lisa Sampson on 5/17/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+enum Appearance {
+    
+}
---
+++ Sprint 6 Challenge/Sprint 6 Challenge/Controls and UI/LockControl.swift
@@ -0,0 +1,21 @@
+//
+//  LockControl.swift
+//  Sprint 6 Challenge
+//
+//  Created by Lisa Sampson on 5/17/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+class LockControl: UIControl {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}

6ee2e81775b7d787f8f27f44edaaf5aa3e0176ee
Michael R.F <mrflowers22@uwalumni.com>
2019-05-17 12:12:25 -0400

    created them helper class

--- Sprint 6 Challenge/Sprint 6 Challenge/CustomControl.swift
+++ Sprint 6 Challenge/Sprint 6 Challenge/CustomControl.swift
@@ -13,13 +13,6 @@ class CustomControl: UIControl {
     //we want to set the value of the x position equal to that of the control's value changed value
     var xValue = 0.0
     
-    //create your uiView inside the custom control
-    
-    required init?(coder aCoder: NSCoder) {
-        super.init(coder: aCoder)
-        setup()
-    }
- 
     override func beginTracking(_ touch: UITouch, with event: UIEvent?) -> Bool {
        updateValue(for: touch)
         sendActions(for: .touchDown)
@@ -61,21 +54,4 @@ class CustomControl: UIControl {
         xValue = Double(xRange)
         sendActions(for: .valueChanged)
     }
-    
-    func setup(){
-        let thumbnail = UIView()
-        //origin: the x and y position
-        let origin = CGPoint(x: 19, y: 0)
-        //size: the width and height
-        let size = CGSize(width: 40, height: bounds.height)
-        //set the frame of the view or the cgrect
-        thumbnail.frame = CGRect(origin: origin, size: size)
-        thumbnail.backgroundColor = .black
-        thumbnail.layer.borderColor = UIColor.white.cgColor
-        thumbnail.layer.borderWidth = 2.0
-        thumbnail.layer.cornerRadius = bounds.width / 2
-        //add it inside of the control
-        addSubview(thumbnail)
-    }
-
 }

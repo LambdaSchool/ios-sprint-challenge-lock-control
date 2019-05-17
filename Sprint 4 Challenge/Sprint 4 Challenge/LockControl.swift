baeb61ca9561f058aa68e8e20b29ee060c6b1d0b
Sameera Leola <pixelandbyte@gmail.com>
2019-05-17 14:55:25 -0500

    Started over after Sprint was done

---
+++ Sprint 4 Challenge/Sprint 4 Challenge/LockControl.swift
@@ -0,0 +1,82 @@
+//
+//  LockControl.swift
+//  Sprint 4 Challenge
+//
+//  Created by Sameera Roussi on 5/17/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+class LockControl: UIControl {
+    
+    var blackDot = "⚫️"
+    var value = 0
+ //   private let upperThumbImageView = UIImageView()
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        
+        clipsToBounds = true
+        let radius: CGFloat = 22
+        layer.cornerRadius = radius
+//        layer.borderWidth = 5
+//        layer.borderColor = UIColor.red.cgColor
+    }
+
+    
+    // MARK: Touch Tracking
+    
+    override func beginTracking(_ touch: UITouch, with event: UIEvent?) -> Bool {
+        let touchPoint = touch.location(in: self)
+        if bounds.contains(touchPoint) {
+            value = Int(touchPoint.x)
+            sendActions(for: [.touchDragInside, .valueChanged])
+        } else {
+            sendActions(for: [.touchDragOutside])
+        }
+        
+        return true
+    }
+    
+    override func continueTracking(_ touch: UITouch, with event: UIEvent?) -> Bool {
+        let touchPoint = touch.location(in: self)
+        if bounds.contains(touchPoint) {
+            value = Int(touchPoint.x)
+//            print(touchPoint.x)
+            sendActions(for: [.touchDragInside, .valueChanged])
+        } else {
+            sendActions(for: [.touchDragOutside])
+        }
+        
+        return true
+    }
+    
+    override func endTracking(_ touch: UITouch?, with event: UIEvent?) {
+        defer {
+            super.endTracking(touch, with: event)
+        }
+        
+        guard let touch = touch else { return }
+        
+        let touchPoint = touch.location(in: self)
+        if bounds.contains(touchPoint) {
+            sendActions(for: [.touchDragInside, .valueChanged])
+        } else {
+            sendActions(for: [.touchDragOutside])
+        }
+    }
+    
+    override func cancelTracking(with event: UIEvent?) {
+        sendActions(for: [.touchCancel])
+    }
+    
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
--- Sprint 4 Challenge/Sprint 4 Challenge/ViewController.swift
+++ Sprint 4 Challenge/Sprint 4 Challenge/ViewController.swift
@@ -12,11 +12,45 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        setup()
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func unlockSliderControl(_ unLockSlider: LockControl) {
+        if (unLockSlider.value >= 192) {
+            lockImageView.image = UIImage(named: "Unlocked")
+        } else {
+            
+        }
     }
+    
+    
+    @IBAction func resetButtonTapped(_ sender: Any) {
+    }
+    
+    private func setup() {
+        grayBackgoundView.layer.cornerRadius = 22
+    //    unlockSliderView.layer.cornerRadius = 22
+        self.navigationItem.title = "Hello!"
+    }
+    
+//    @objc func springButtonTapped() {
+//        label.center = self.view.center
+//        self.label.transform = CGAffineTransform(scaleX: 0.0001, y: 0.0001)
+//        UIView.animate(withDuration: 3.0,
+//                       delay: 0,
+//                       usingSpringWithDamping: 0.3,
+//                       initialSpringVelocity: 0,
+//                       options: [],
+//                       animations: {
+//                        self.label.transform = .identity
+//        },
+//                       completion: nil)
+//    }
+    
+    
+    @IBOutlet weak var grayBackgoundView: UIView!
+    @IBOutlet weak var lockImageView: UIImageView!
+    @IBOutlet weak var unlockSliderView: LockControl!
 }

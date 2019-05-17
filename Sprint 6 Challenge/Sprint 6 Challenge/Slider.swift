8929f83c703d0ff40bb6bff38d3754af7df76de0
victor <ruizdvictor@gmail.com>
2019-05-17 12:45:44 -0400

    added slider to view

---
+++ Sprint 6 Challenge/Sprint 6 Challenge/Slider.swift
@@ -0,0 +1,13 @@
+//
+//  Slider.swift
+//  Sprint 6 Challenge
+//
+//  Created by Victor  on 5/17/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+import Foundation
+import UIKit
+
+class slider: UISlider {
+    
+}

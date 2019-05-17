13fdf6ddc8575a63985530650040b97ae4f94e48
hectorsvill <hectorstevenvillasano@gmail.com>
2019-05-17 08:26:03 -0700

    SliderControl

--- Sprint 6 Challenge/Sprint 6 Challenge/ViewController.swift
+++ Sprint 6 Challenge/Sprint 6 Challenge/ViewController.swift
@@ -9,12 +9,22 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+	
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        title = "Hello!"
+		
+		sliderContainerView.layer.cornerRadius = 28
+		mainView.layer.cornerRadius = 22
+		
     }
-
+	
+	
+	@IBAction func resetBarButton(_ sender: Any) {
+	}
+	
+	@IBOutlet var sliderContainerView: UIView!
+	@IBOutlet var mainView: UIView!
 
 }
 

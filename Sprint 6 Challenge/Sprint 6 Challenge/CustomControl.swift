1ad8570279a8da5e3b57a30d1939e4737ea115f3
Christopher Aronson <christopheraronson@icloud.com>
2019-05-17 09:43:06 -0700

    Custom Control is visible on interface

--- Sprint 6 Challenge/Sprint 6 Challenge/CustomControl.swift
+++ Sprint 6 Challenge/Sprint 6 Challenge/CustomControl.swift
@@ -9,7 +9,49 @@
 import UIKit
 
 class CustomControl: UIControl {
+    var value = 0.0
+    
+    private let thumbViewSize: CGFloat = 60
+    private let cornerRadius: CGFloat = 30
+    private let thumbViewColor = UIColor.black
+    
+    private var thumbView: UIView?
+    
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        
+        setup()
+    }
+    
+    // MARK: - Private Functions
+    
+    private func setup() {
+        
+        thumbView = UIView(frame: CGRect(x: 8, y: 7, width: thumbViewSize, height: thumbViewSize))
+        thumbView?.layer.cornerRadius = cornerRadius
+        thumbView?.backgroundColor = thumbViewColor
+        
+        addSubview(thumbView!)
+    }
+    
+    // MARK: - Touch Handlers
+    
+    override func beginTracking(_ touch: UITouch, with event: UIEvent?) -> Bool {
+        return true
+    }
+    
+    override func continueTracking(_ touch: UITouch, with event: UIEvent?) -> Bool {
+        return true
+    }
+    
+    override func endTracking(_ touch: UITouch?, with event: UIEvent?) {
+    }
+    
+    override func cancelTracking(with event: UIEvent?) {
 
+    }
+    
     /*
     // Only override draw() if you perform custom drawing.
     // An empty implementation adversely affects performance during animation.
--- Sprint 6 Challenge/Sprint 6 Challenge/ViewController.swift
+++ Sprint 6 Challenge/Sprint 6 Challenge/ViewController.swift
@@ -21,5 +21,8 @@ class ViewController: UIViewController {
     }
 
 
+    @IBAction func updateLock(_ slider: CustomControl) {
+        print(slider.value)
+    }
 }
 

d51cd2ced3658326ee181d390864fbf37b78dd8c
victor <ruizdvictor@gmail.com>
2019-05-17 13:27:23 -0400

    added container view to storyboard

--- Sprint 6 Challenge/Sprint 6 Challenge/ViewController.swift
+++ Sprint 6 Challenge/Sprint 6 Challenge/ViewController.swift
@@ -9,30 +9,13 @@
 import UIKit
 
 class ViewController: UIViewController {
-    
-    let unlockedImage = UIImage(named: "Unlocked")!
-    var animationArray: [UIImage] = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        createSlider()
     }
     
-    func createSlider() {
-        let slider = UISlider()
-        slider.frame = CGRect(x: 0, y: 0, width: 250, height: 35)
-        slider.center = self.view.center
-        slider.minimumTrackTintColor = .green
-        slider.maximumTrackTintColor = .red
-        slider.thumbTintColor = .black
-        slider.maximumValue = 100
-        slider.minimumValue = 0
-        slider.setValue(0, animated: false)
-        slider.transform = CGAffineTransform(scaleX: 0.85, y: 0.85)
-        slider.isContinuous = true
-        self.view.addSubview(slider)
-    }
+  
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()

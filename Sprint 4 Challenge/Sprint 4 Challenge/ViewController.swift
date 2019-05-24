c1bc229bc60a229ff2d15082c46c623b7b14ded2
Ryan Murphy <thomasryanmurphy@gmail.com>
2019-05-24 13:49:31 -0400

    I have it running ... committing before I mess something up ...

--- Sprint 4 Challenge/Sprint 4 Challenge/ViewController.swift
+++ Sprint 4 Challenge/Sprint 4 Challenge/ViewController.swift
@@ -10,13 +10,65 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var resetButton: UIBarButtonItem!
+    @IBOutlet weak var lockSetup: LockSetup!
+    
+    func updateResetButton() {
+        if lockSetup.locked == true {
+            resetButtonNotEnabled()
+        } else {
+            resetButtonIsEnabled()
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        updateResetButton()
     }
-
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(true)
+//        updateResetButton()
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+//
+//    @IBAction func resetEnableAction(_ sender: LockSetup) {
+//        if sender.locked {
+//            resetButtonNotEnabled()
+//        } else {
+//            resetButtonIsEnabled()
+//        }
+//
+//    }
+    
+    @IBAction func resetButtonPushed(_ sender: Any) {
+        lockSetup.reset()
+    }
+    
+    @IBAction func resetButtonEnabledToggle(_ sender: Any) {
+        updateResetButton()
+    }
+    
+    func resetButtonIsEnabled() {
+        
+    self.resetButton.isEnabled = true
+    self.resetButton.tintColor = .black
+        
+    }
+    
+    func resetButtonNotEnabled() {
+        
+    self.resetButton.isEnabled = false
+    self.resetButton.tintColor = .clear
+        
+    }
+    
+
+    
+    
+    
+    
 }

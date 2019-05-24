f7b59b875d687a25ae1d98cda1c8501c2784bab8
Mitchell Budge <mitchellbudge@gmail.com>
2019-05-24 11:36:59 -0600

    Functional app, 1 caveat
    Need to work on getting the ball to go back to the initial starting point after pressing the reset button

---
+++ Sprint 4 Challenge/Sprint 4 Challenge/CustomControl.swift
@@ -0,0 +1,141 @@
+//
+//  CustomControl.swift
+//  Sprint 4 Challenge
+//
+//  Created by Mitchell Budge on 5/24/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable class CustomControl: UIControl {
+
+    var x = 0.0
+    var minimumValue: CGFloat = 0
+    var maximumValue: CGFloat = 1
+    var ballValue: CGFloat = 0.0
+    var isUnlocked: Bool = false
+    
+    private var ballView = UIView()
+    private var previousLocation = CGPoint()
+    private var ballWidth: CGFloat {
+        return frame.height * 0.825
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        setupBall(ballView)
+    }
+    
+    private func setupBall(_ ball: UIView) {
+        let ballFrame = CGRect(x: 2, y: 4, width: ballWidth, height: ballWidth)
+        ball.frame = ballFrame
+        ball.backgroundColor = .black
+        ball.layer.cornerRadius = ballWidth / 2
+        addSubview(ball)
+        ball.isUserInteractionEnabled = false
+    }
+    
+    private func updateFrames() {
+        ballView.frame = ballFrame(for: ballValue)
+    }
+    
+    func position(for value: CGFloat) -> CGFloat {
+        return (bounds.width - ballWidth - 10) * value
+    }
+    
+    private func ballFrame(for value:CGFloat) -> CGRect {
+        let x = position(for: value) + 6
+        let y = (bounds.height - ballWidth) / 2
+        return CGRect(x: x, y: y, width: ballWidth, height: ballWidth)
+    }
+    
+    override func beginTracking(_ touch: UITouch, with event: UIEvent?) -> Bool {
+        previousLocation = touch.location(in: self)
+        if ballView.frame.contains(previousLocation) {
+            sendActions(for: [.touchDown, .valueChanged])
+        } else {
+            return false
+        }
+        return true
+    }
+    
+    override func continueTracking(_ touch: UITouch, with event: UIEvent?) -> Bool {
+        let location = touch.location(in: self)
+        let locationChange = location.x - previousLocation.x
+        let valueChange = (maximumValue - minimumValue) * locationChange / bounds.width
+        previousLocation = location
+        ballValue = min(max((ballValue + valueChange), minimumValue), maximumValue)
+        
+        updateFrames()
+        
+        return true
+    }
+    
+    override func endTracking(_ touch: UITouch?, with event: UIEvent?) {
+        defer { super.endTracking(touch, with: event) }
+        guard let touch = touch else { return }
+        previousLocation = touch.location(in: self)
+        if ballValue >= 0.8 {
+            ballValue = 1
+            isUnlocked = true
+            sendActions(for: [.touchUpInside, .valueChanged])
+            UIView.animate(withDuration: 1.0) {
+                self.ballView.frame = self.ballFrame(for: 1.0)
+            }
+        }   else {
+            sendActions(for: [.touchUpInside, .valueChanged])
+            isUnlocked = false
+            ballValue = 0
+            UIView.animate(withDuration: 2.0) {
+                self.ballView.frame = self.ballFrame(for: 0.0)
+            }
+        }
+        updateFrames()
+    }
+    
+    override func cancelTracking(with event: UIEvent?) {
+        super.cancelTracking(with: event)
+    }
+    
+    /*func updateValue(at touch: UITouch){
+        let touchpoint = touch.location(in: self)
+        let xBounds = touchpoint.x / bounds.width
+        x = Double(xBounds)
+        sendActions(for: [.valueChanged])
+    }
+    
+    override func beginTracking(_ touch: UITouch, with event: UIEvent?) -> Bool {
+        updateValue(at: touch)
+        sendActions(for: [.touchDown])
+        return true
+    }
+    
+    override func continueTracking(_ touch: UITouch, with event: UIEvent?) -> Bool {
+        let touchpoint = touch.location(in: self)
+        if bounds.contains(touchpoint) {
+            sendActions(for: [.touchDragInside])
+        } else {
+            sendActions(for: [.touchDragOutside])
+        }
+        updateValue(at: touch)
+        return true
+    }
+    
+    override func endTracking(_ touch: UITouch?, with event: UIEvent?) {
+        guard let touch = touch else { return }
+        let touchpoint = touch.location(in: self)
+        if bounds.contains(touchpoint) {
+            sendActions(for: [.touchUpInside, .valueChanged])
+        } else {
+            sendActions(for: [.touchUpOutside, .valueChanged])
+        }
+        updateValue(at: touch)
+    }
+    
+    override func cancelTracking(with event: UIEvent?) {
+        sendActions(for: [.touchCancel, .valueChanged])
+    }
+    */
+}
+
--- Sprint 4 Challenge/Sprint 4 Challenge/ViewController.swift
+++ Sprint 4 Challenge/Sprint 4 Challenge/ViewController.swift
@@ -12,26 +12,54 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        lockImageView.image = UIImage(named: "Locked")
+        initialSetup()
         
     }
 
+    @IBOutlet weak var resetButton: UIBarButtonItem!
     @IBOutlet weak var masterView: UIView!
     @IBOutlet weak var lockImageView: UIImageView!
-    @IBOutlet weak var controlView: UIView!
-    @IBOutlet weak var ballView: UIView!
+    @IBOutlet weak var customControlView: CustomControl!
+    
+    let cornerCurve = CGFloat(25)
+    
+    func initialSetup() {
+        lockImageView.image = UIImage(named: "Locked")
+        masterView.layer.cornerRadius = cornerCurve
+        masterView.backgroundColor = AppearanceHelper.offWhite
+        lockImageView.layer.cornerRadius = cornerCurve
+        customControlView.layer.cornerRadius = cornerCurve / 2
+        customControlView.backgroundColor = AppearanceHelper.lambdaMaroon
+        resetButton.tintColor = .clear
+    }
+    
+    @IBAction func touchUnlock(_ sender: CustomControl) {
+        if customControlView.ballValue > 0.80 {
+            unlockAnimation()
+            customControlView.isEnabled = false
+        }
+    }
     
     @IBAction func resetButtonPressed(_ sender: Any) {
-        resetButton()
+        reset()
     }
     
     func unlockAnimation() {
-        title = "Unlocked!"
+        UIView.animate(withDuration: 10) {
+            self.lockImageView.image = UIImage(named: "Unlocked")
+            self.title = "Unlocked!"
+            self.resetButton.tintColor = AppearanceHelper.offWhite
+        }
     }
     
     
-    func resetButton() {
-        title = "Locked"
+    func reset() {
+        UIView.animate(withDuration: 10) {
+            self.lockImageView.image = UIImage(named: "Locked")
+            self.title = "Locked"
+            self.resetButton.tintColor = .clear
+            self.customControlView.isEnabled = true
+            
+        }
     }
-    
 }

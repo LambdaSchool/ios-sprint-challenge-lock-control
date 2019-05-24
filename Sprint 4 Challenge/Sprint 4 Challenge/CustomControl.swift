f05d6fc1f0e162dd5cc915b8ee13958ed92ce984
Jeremy Taylor <jtaylor@jctaylor.org>
2019-05-24 10:40:47 -0500

    Basic Storyboard and Themeing done

---
+++ Sprint 4 Challenge/Sprint 4 Challenge/CustomControl.swift
@@ -0,0 +1,13 @@
+//
+//  CustomControl.swift
+//  Sprint 4 Challenge
+//
+//  Created by Jeremy Taylor on 5/24/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable class CustomControl: UIControl {
+    
+}
--- Sprint 4 Challenge/Sprint 4 Challenge/ViewController.swift
+++ Sprint 4 Challenge/Sprint 4 Challenge/ViewController.swift
@@ -9,14 +9,26 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    // MARK: - Properties
+    @IBOutlet weak var lockImageView: UIImageView!
+    @IBOutlet weak var customControl: CustomControl!
+    @IBOutlet weak var resetButton: UIBarButtonItem!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        resetButton.title = ""
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func lockControlValueChanged(_ sender: CustomControl) {
+        
+    }
+    
+    @IBAction func reset(_ sender: UIBarButtonItem) {
+        lockImageView.image = UIImage(named: "Locked")
+        UIView.animate(withDuration: 2.0) {
+            self.resetButton.tintColor = UIColor.black.withAlphaComponent(0)
+        }
+        navigationItem.title = "Slide to Unlock"
     }
 }

c85047e44aa14b971106a6e5b5c3237811a60d9b
Hayden Hastings <haydenhastings32@gmail.com>
2019-05-24 11:47:36 -0600

    Finished the app.

--- Sprint 4 Challenge/Sprint 4 Challenge/ViewController.swift
+++ Sprint 4 Challenge/Sprint 4 Challenge/ViewController.swift
@@ -10,13 +10,58 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let cornerCurve = CGFloat(25)
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        initalSetUp()
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func initalSetUp() {
+        lockImage.image = UIImage(named: "Locked")
+        mainView.layer.cornerRadius = cornerCurve
+        lockImage.layer.cornerRadius = cornerCurve
+        sliderTrackView.layer.cornerRadius = cornerCurve / 2
+        resetButton.tintColor = .clear
     }
+    
+    func reset() {
+        
+        sliderTrackView.reset()
+        
+        UIView.animate(withDuration: 10) {
+            self.lockImage.image = UIImage(named: "Locked")
+            self.title = "Locked"
+            self.resetButton.tintColor = .clear
+            self.sliderTrackView.isEnabled = true
+        }
+    }
+    
+    func unlockAnimation() {
+        UIView.animate(withDuration: 10) {
+            self.lockImage.image = UIImage(named: "Unlocked")
+            self.title = "Unlocked!"
+            self.resetButton.tintColor = ApperanceHelper.yellowColor
+        }
+    }
+    
+    @IBAction func resetButtonTapped(_ sender: Any) {
+        reset()
+    }
+    
+    @IBAction func sliderUnlocked(_ sender: UnlockSlider) {
+        if sender.ballValue > 0.80 {
+            unlockAnimation()
+            sender.ballValue = 1
+            sliderTrackView.isEnabled = false
+        }
+    }
+    
+    
+    @IBOutlet weak var mainView: UIView!
+    @IBOutlet weak var sliderTrackView: UnlockSlider!
+    
+    @IBOutlet weak var resetButton: UIBarButtonItem!
+    @IBOutlet weak var lockImage: UIImageView!
 }

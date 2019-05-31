89e5b52bf3ebd43fad05f3e0c8e50ad6895610c1
Thomas Cacciatore <tcacciatore597@gmail.com>
2019-05-31 13:41:02 -0400

    App is mostly functional.  Can't figure out how to reset my slider position.

---
+++ Sprint 4 Challenge/Sprint 4 Challenge/SliderLayer.swift
@@ -0,0 +1,13 @@
+//
+//  SliderLayer.swift
+//  Sprint 4 Challenge
+//
+//  Created by Thomas Cacciatore on 5/31/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+class SliderLayer: CALayer {
+    weak var customControl: CustomControl?
+}
--- Sprint 4 Challenge/Sprint 4 Challenge/ViewController.swift
+++ Sprint 4 Challenge/Sprint 4 Challenge/ViewController.swift
@@ -18,16 +18,40 @@ class ViewController: UIViewController {
 
     func setupView() {
         outsideView.layer.cornerRadius = 20
+        customSliderView.layer.cornerRadius = 10
         if let button = self.navigationItem.rightBarButtonItem {
             button.isEnabled = false
             button.tintColor = UIColor.clear
+        imageView.image = UIImage(named: "Locked")
         }
     }
     
+    @IBAction func resetButtonTapped(_ sender: Any) {
+        
+        UIView.animate(withDuration: 0.5) {
+            self.setupView()
+        
+        }
+    }
+    
+    
+    @IBAction func sliderValueChanged(_ sender: CustomControl) {
+        if sender.lowerValue >= 0.9 {
+            UIView.animate(withDuration: 0.50) {
+                self.imageView.image = UIImage(named: "Unlocked")
+                self.navigationItem.rightBarButtonItem?.isEnabled = true
+                self.navigationItem.rightBarButtonItem?.tintColor = AppearanceHelper.specialGreen
+            }
+        } else {
+            imageView.image = UIImage(named: "Locked")
+        }
+    }
+    
+    
+    @IBOutlet weak var customSliderView: UIView!
     @IBOutlet weak var outsideView: UIView!
     @IBOutlet weak var imageView: UIImageView!
     @IBOutlet weak var resetButton: UIBarButtonItem!
-    
     
     
 }

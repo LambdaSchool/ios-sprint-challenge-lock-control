423b49c73ad009bf069831734eea94a46d159972
John Pitts <jgpitts@gmail.com>
2019-05-31 19:41:28 -0400

    completed a working button and navigation area theme. next need to work the lock animation.

---
+++ Sprint 4 Challenge/Sprint 4 Challenge/RangeSliderTrackLayer.swift
@@ -0,0 +1,14 @@
+//
+//  RangeSliderTrackLayer.swift
+//  Sprint 4 Challenge
+//
+//  Created by John Pitts on 5/31/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+class RangeSliderTrackLayer: CALayer {
+    
+    weak var rangeSlider: RangeSlider?
+}
--- Sprint 4 Challenge/Sprint 4 Challenge/ViewController.swift
+++ Sprint 4 Challenge/Sprint 4 Challenge/ViewController.swift
@@ -9,57 +9,21 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        //layer.addSublayer(setUpGraySquare())   why won't this work?
-        rangeSlider?.intrinsicContentSize
-    }
-    
-    @IBAction func resetLock(_ sender: Any) {
-        
-        
-        
-        // 1.  send lock animation back to identity
-        // 2.  rangeSlider ball goes back to zero
-        // 3.  vanish the Reset button
-    }
-    
-    @IBAction func openLockSlider(_ ratingControl: RangeSlider) {
-        
-        // if value >= 0.8 { openLockAnimation AND
-        // enable "Reset" button to appear in NavBar
-        // else { go back to identity
-        
-        
     }
 
-    func setUpGraySquare() {
+    
+    @IBAction func rangeChanged(_ sender: Any) {
         
-        let graySquare = UIView()
-        graySquare.backgroundColor = .gray
-        graySquare.clipsToBounds = true
-        graySquare.layer.cornerRadius = 32
-        graySquare.translatesAutoresizingMaskIntoConstraints = false
         
-        self.view.addSubview(graySquare)
         
-        let grayLeadingConstraint = graySquare.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 40)
-        let grayBottomConstraint = graySquare.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -256)
-        let grayHeightConstraint = graySquare.heightAnchor.constraint(equalToConstant: 320)
-        let grayAspectRatioConstraint = graySquare.widthAnchor.constraint(equalTo: graySquare.heightAnchor, multiplier: 1.0, constant: 0)
-
-        
-        NSLayoutConstraint.activate(
-            [
-                grayLeadingConstraint,
-                grayBottomConstraint,
-                grayHeightConstraint,
-                grayAspectRatioConstraint
-            ])
     }
+    
+    
     
     @IBOutlet var rangeSlider: RangeSlider!
-    var graySquare: UIView?
+    
 }

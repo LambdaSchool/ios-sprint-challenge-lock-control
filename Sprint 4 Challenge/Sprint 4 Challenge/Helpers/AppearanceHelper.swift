a4f1ab7eb01b90501c9272206fc491ce268f4103
seanacres <6416690+seanacres@users.noreply.github.com>
2019-07-19 11:05:59 -0400

    Set up project structure, storyboard and files

---
+++ Sprint 4 Challenge/Sprint 4 Challenge/Helpers/AppearanceHelper.swift
@@ -0,0 +1,9 @@
+//
+//  AppearanceHelper.swift
+//  Sprint 4 Challenge
+//
+//  Created by Sean Acres on 7/19/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import Foundation
---
+++ Sprint 4 Challenge/Sprint 4 Challenge/Views/LockControl.swift
@@ -0,0 +1,16 @@
+//
+//  LockControl.swift
+//  Sprint 4 Challenge
+//
+//  Created by Sean Acres on 7/19/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class LockControl: UIControl {
+    
+    
+    
+}

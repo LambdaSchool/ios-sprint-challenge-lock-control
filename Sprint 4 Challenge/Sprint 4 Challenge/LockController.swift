00b5a6cd20f55eef1baace5293736f147672b394
Nate Hedgeman <nate.hedgeman@gmail.com>
2019-07-19 15:50:17 -0400

    finished tracking controls and LockController

--- Sprint 4 Challenge/Sprint 4 Challenge/LockController.swift
+++ Sprint 4 Challenge/Sprint 4 Challenge/LockController.swift
@@ -14,13 +14,13 @@ class LockController: UIControl {
     //Properties
     
     var isUnlocked: Bool = false
-    let minValue: Double = 0
-    let maxValue: Double = 1
-    var lockSliderValue: Double = 0
+    let minValue: CGFloat = 0
+    let maxValue: CGFloat = 1
+    var lockSliderValue: CGFloat = 0
     
+    var currentLocation = CGPoint()
+    var previousLocation = CGPoint()
     
-    
-    var sliderView = UIView()
     var lockSliderView = UIView()
     
     
@@ -52,10 +52,19 @@ class LockController: UIControl {
     
     func updateSlider() {
         
-        sliderView.frame = getSliderRect(for: CGFloat(lockSliderValue))
+        lockSliderView.frame = getSliderRect(for: CGFloat(lockSliderValue))
         
     }
     
+    
+    func getSliderRect(for value:CGFloat) -> CGRect {
+        let x = (bounds.width - lockSliderHeight) / 6.67
+        let y = (bounds.height - lockSliderWidth) / 2
+        
+        return CGRect(x: x, y: y, width: lockSliderWidth, height: lockSliderWidth)
+    }
+    
+    //To reset the lock
     func reset() {
         
         lockSliderValue = 0.0
@@ -64,15 +73,72 @@ class LockController: UIControl {
         
     }
     
-    func getSliderRect(for value:CGFloat) -> CGRect {
-        let x = (bounds.width)
-        let y = (bounds.height - lockSliderWidth) / 2
+    
+    
+    //Tracking Controls
+    override func beginTracking(_ touch: UITouch, with event: UIEvent?) -> Bool {
         
-        return CGRect(x: x, y: y, width: lockSliderWidth, height: lockSliderWidth)
+        let previousLocation = touch.location(in: self)
+        
+        if lockSliderView.frame.contains(previousLocation) {
+            
+            sendActions(for: [.touchDown, .valueChanged])
+        }
+        
+        return true
     }
     
+    override func continueTracking(_ touch: UITouch, with event: UIEvent?) -> Bool {
+        
+        currentLocation = touch.location(in: self)
+        
+        let locationChange = currentLocation.x - previousLocation.x
+        let valueChange = (maxValue - minValue) * locationChange / bounds.width
+        
+        previousLocation = currentLocation
+        
+        lockSliderValue = min(max((lockSliderValue + valueChange), minValue), maxValue)
+        
+        updateSlider()
+        
+        return true
+    }
     
+    override func endTracking(_ touch: UITouch?, with event: UIEvent?) {
+        defer { super.endTracking(touch, with: event) }
+        
+        guard let touch = touch else { return }
+        
+        currentLocation = touch.location(in: self)
+        
+        
+        if lockSliderValue >= 0.8 {
+            
+            lockSliderValue = 1
+            isUnlocked = true
+            sendActions(for: [.touchUpInside, .valueChanged])
+            
+            UIView.animate(withDuration: 0.2) {
+                self.lockSliderView.frame = self.getSliderRect(for: 1.0)
+                
+            }
+            
+        }   else {
+            
+            lockSliderValue = 0
+            isUnlocked = false
+            sendActions(for: [.touchUpInside, .valueChanged])
+            
+            UIView.animate(withDuration: 0.5) {
+                
+                self.lockSliderView.frame = self.getSliderRect(for: 0.0)
+            }
+        }
+        
+        updateSlider()
+    }
     
-    
-    
+    override func cancelTracking(with event: UIEvent?) {
+        super.cancelTracking(with: event)
+    }
 }

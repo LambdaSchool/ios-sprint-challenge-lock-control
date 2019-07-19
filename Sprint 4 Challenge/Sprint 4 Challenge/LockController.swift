b2f320f39925f4d0f28d76637369227604095bfb
Nate Hedgeman <nate.hedgeman@gmail.com>
2019-07-19 14:13:43 -0400

    add slider function for update and slider rect (as far as I got)

--- Sprint 4 Challenge/Sprint 4 Challenge/LockController.swift
+++ Sprint 4 Challenge/Sprint 4 Challenge/LockController.swift
@@ -18,16 +18,27 @@ class LockController: UIControl {
     let maxValue: Double = 1
     var lockSliderValue: Double = 0
     
+    
+    var sliderView = UIView()
+    var lockSliderView = UIView()
+    
+    
     var lockSliderWidth: CGFloat {
         
         return frame.height * 0.825
     }
     
-    var lockSliderView = UIView()
+    var lockSliderHeight: CGFloat {
+        
+        return frame.width * 0.15
+    }
+    
+    
     
     
     func createSlider(_ slider: UIView) {
         let sliderRect = CGRect(x: 5, y: 5, width: lockSliderWidth, height: lockSliderWidth)
+        
         slider.frame = sliderRect
         slider.backgroundColor = .black
         slider.layer.cornerRadius = lockSliderWidth / 2
@@ -35,6 +46,18 @@ class LockController: UIControl {
         addSubview(slider)
         
         slider.isUserInteractionEnabled = false
+    }
+    
+    func updateSlider() {
+        
+        sliderView.frame = getSliderRect(for: CGFloat(lockSliderValue))
+        
+    }
+    
+    func getSliderRect(for value:CGFloat) -> CGRect {
+        let x = (bounds.width)
+        let y = (bounds.height - lockSliderWidth) / 2
+        return CGRect(x: x, y: y, width: lockSliderWidth, height: lockSliderWidth)
     }
     
     
